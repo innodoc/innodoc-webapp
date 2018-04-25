10fb334829ad05b21de77ffc4eefec09aabf277f
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-04-25 12:42:21 +0200

    refactor Math

--- components/content/Math.js
+++ components/content/Math.js
@@ -3,26 +3,6 @@ import PropTypes from 'prop-types'
 
 import BaseContentComponent from './Base'
 
-class InlineMath extends React.Component {
-  static propTypes = {
-    texCode: PropTypes.string.isRequired
-  }
-
-  render() {
-    return <span className="math inline">\({this.props.texCode}\)</span>
-  }
-}
-
-class DisplayMath extends React.Component {
-  static propTypes = {
-    texCode: PropTypes.string.isRequired
-  }
-
-  render() {
-    return <span className="math display">\[{this.props.texCode}\]</span>
-  }
-}
-
 export default class Math extends BaseContentComponent {
   static propTypes = {
     data: PropTypes.array.isRequired
@@ -31,13 +11,22 @@ export default class Math extends BaseContentComponent {
   render() {
     const mathType = this.props.data[0].t
     const texCode = this.props.data[1]
+    const classNames = ['math']
+    let delims
 
-    if (mathType === 'InlineMath')
-      return <InlineMath texCode={texCode} />
+    if (mathType === 'InlineMath') {
+      classNames.push('inline')
+      delims = ['\\(', '\\)']
+    }
+    else if (mathType === 'DisplayMath') {
+      classNames.push('display')
+      delims = ['\\[', '\\]']
+    }
+    else {
+      throw new Error(
+        `mathType must be either InlineMath or DisplayMath. Got ${mathType}!`)
+    }
 
-    if (mathType === 'DisplayMath')
-      return <DisplayMath texCode={texCode} />
-
-    throw new Error(`mathType must be either InlineMath or DisplayMath. Got ${mathType}!`)
+    return <span className={classNames}>{delims[0]}{texCode}{delims[1]}</span>
   }
 }
