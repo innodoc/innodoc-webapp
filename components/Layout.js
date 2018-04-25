bd293abe614bfa982ad08a775c6fc25e82c2b536
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-04-25 12:31:04 +0200

    custom head tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Head from 'next/head'
+
 import Header from './Header'
 
 export default class Layout extends React.Component {
@@ -14,6 +16,10 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
+        <Head>
+          <title key="title">innoDoc web app</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Header />
         {this.props.children}
       </div>
