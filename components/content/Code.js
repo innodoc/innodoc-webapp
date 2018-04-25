742458bdcf98f9015b98cbd6a731a27b8595fae1
Niklas <contact@niklasplessing.net>
2018-04-25 19:17:27 +0200

    init exercises

--- components/content/Code.js
+++ components/content/Code.js
@@ -2,16 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import BaseContentComponent from './Base'
+import {InputQuestionComponent, MathInputQuestionComponent} from '../questions/Input'
 
 export default class Code extends BaseContentComponent {
+
   static propTypes = {
     data: PropTypes.array.isRequired
   }
 
+  constructor(props) {
+    super(props)
+    const [id, classNames, attrs] = props.data[0]
+    this.id = id
+    this.classNames = classNames
+    this.attrs = this.attrsToObj(attrs)
+    this.content = this.props.data[1]
+    this.solution = this.attrs.solution
+  }
+
+  attrsToObj(attrs) {
+    let a = attrs.reduce((obj, item) => {
+      obj[item[0]] = item[1]
+      return obj
+    }, {})
+    return a
+  }
+
+  getQuestionComponent(classNames, attrs) {
+
+    if (attrs.validator == 'math') {
+      return <MathInputQuestionComponent solution={this.solution} attrs={attrs}/>
+    }
+
+    return <InputQuestionComponent solution={this.solution} attrs={attrs}/>
+  }
+
   render() {
-    const content = this.props.data[1]
+    if (this.classNames.includes('exercise')) {
+      return this.getQuestionComponent(this.classNames, this.attrs)
+    }
+
     return (
-      <code>{content}</code>
+      <code id={this.id} className={this.classNames}>{this.content}</code>
     )
   }
 }
---
+++ components/questions/Input.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+var math = require('mathjs');
+
+import BaseContentComponent from '../content/Base'
+
+
+class QuestionComponent extends BaseContentComponent{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      points: 0,
+      inputValue: '',
+      solved: false,
+      color: 'transparent'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  update() {
+
+    let solved = this.validate()
+
+    // debugging color
+    let color
+    if (this.state.inputValue == '') {
+      color = 'transparent'
+    } else if (solved) {
+      color = 'green'
+    } else {
+      color = 'red'
+    }
+
+    this.setState({
+      color: color,
+      solved: solved
+    })
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value}, () => {
+      this.update()
+    });
+  }
+}
+
+class InputQuestionComponent extends QuestionComponent {
+
+
+  static propTypes = {
+    solution: PropTypes.string.isRequired,
+  }
+
+  validate() {
+    const solved = this.state.inputValue == this.props.solution
+    return solved
+  }
+
+  render() {
+    return (
+      <input
+        type='text'
+        className='exercise inputquestion'
+        value={this.state.inputValue}
+        onChange={this.handleChange}
+        style={{ backgroundColor: this.state.color}}/>
+    )
+  }
+}
+
+class MathInputQuestionComponent extends InputQuestionComponent {
+  validate() {
+
+    var epsilon = this.props.attrs.precision
+    epsilon = math.eval('1e-' + epsilon)
+    math = math.create({
+      epsilon: epsilon
+    })
+
+    try{
+      var parsedInput = math.eval(this.state.inputValue)
+      var parsedSolution = math.eval(this.props.solution)
+    } catch (e) {
+      if (e instanceof SyntaxError) {
+        return false
+      }
+    }
+    if (typeof parsedInput !== 'undefined') {
+      return math.equal(parsedInput, parsedSolution)
+    }
+    return false
+  }
+}
+
+// class FunctionInputQuestionComponent extends InputQuestionComponent {
+//
+// }
+
+export {QuestionComponent, InputQuestionComponent, MathInputQuestionComponent}
