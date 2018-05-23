b1a10e2f536e4667c17dcd1eabd96e53928baee4
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-05-23 13:18:04 +0200

    refactor components/Toc

---
+++ components/Toc/Item.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Menu } from 'semantic-ui-react'
+
+import { contentType } from '../../lib/propTypes'
+import SectionLink from '../SectionLink'
+import ContentFragment from '../ContentFragment'
+
+const Item = ({ title, sectionId, subSections }) => {
+  if (!subSections.length) {
+    return (
+      <SectionLink sectionId={sectionId}>
+        <Menu.Item as="a">
+          <ContentFragment content={title} />
+        </Menu.Item>
+      </SectionLink>
+    )
+  }
+  return (
+    <Menu.Item>
+      <SectionLink sectionId={sectionId}>
+        <a>
+          <ContentFragment content={title} />
+        </a>
+      </SectionLink>
+      <Menu.Menu>
+        {
+          subSections.map(
+            (subSection, i) => (
+              <Item
+                title={subSection.title}
+                sectionId={`${sectionId}/${subSection.id}`}
+                subSections={subSection.children}
+                key={i.toString()}
+              />
+            )
+          )
+        }
+      </Menu.Menu>
+    </Menu.Item>
+  )
+}
+
+Item.propTypes = {
+  title: contentType.isRequired,
+  sectionId: PropTypes.string.isRequired,
+  subSections: PropTypes.arrayOf(PropTypes.object),
+}
+
+Item.defaultProps = { subSections: [] }
+
+export default Item
---
+++ components/Toc/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Menu } from 'semantic-ui-react'
+
+import { getToc } from '../../store/selectors'
+import { tocTreeType } from '../../lib/propTypes'
+import TocItem from './Item'
+
+const Toc = ({
+  toc,
+  as: ElementType,
+  dispatch,
+  ...otherProps
+}) => {
+  const sections = toc.map(
+    (section, i) => (
+      <TocItem
+        sectionId={section.id}
+        title={section.title}
+        subSections={section.children}
+        key={i.toString()}
+      />
+    )
+  )
+
+  return (
+    <ElementType {...otherProps}>
+      {sections}
+    </ElementType>
+  )
+}
+
+Toc.propTypes = {
+  toc: tocTreeType.isRequired,
+  as: PropTypes.func.isRequired,
+  dispatch: PropTypes.func,
+}
+
+Toc.defaultProps = {
+  ...React.Component.defaultProps,
+  as: Menu,
+  toc: {},
+}
+
+const mapStateToProps = state => ({ toc: getToc(state) })
+
+export default connect(mapStateToProps)(Toc)
