f3905755d0eb32698db66a7b7c9af6ef05de5600
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-05-23 19:51:28 +0200

    merge another semantic-ui implementation of BulletList, OrderedList

--- components/content/BulletList.js
+++ components/content/BulletList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { List } from 'semantic-ui-react'
+
 import ContentFragment from '../ContentFragment'
 
 const BulletList = ({ data }) => {
--- components/content/OrderedList.js
+++ components/content/OrderedList.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { List } from 'semantic-ui-react'
+
 import ContentFragment from '../ContentFragment'
 
 const OrderedList = ({ data }) => {
+  // TODO: unwrap paragraphs
   const listItems = data[1].map(
     (item, i) => (
-      <List.Item key={i.toString()}>
+      <List.Item as="li" key={i.toString()}>
         <ContentFragment content={item} />
       </List.Item>
     )
   )
   return (
-    <List ordered divided verticalAlign="middle" relaxed="very">
+    <List ordered divided verticalAlign="middle" relaxed="very" as="ol">
       {listItems}
     </List>
   )
