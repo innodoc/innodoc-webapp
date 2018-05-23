38b7f2f1a02336b97ec420fa436393b0859785c0
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-05-23 16:39:23 +0200

    use cached section content

--- sagas/content/section.js
+++ sagas/content/section.js
@@ -1,5 +1,6 @@
-import { fork, put, take } from 'redux-saga/effects'
+import { fork, put, select, take } from 'redux-saga/effects'
 
+import { selectors } from '../../store/reducers/content'
 import {
   actionTypes,
   loadSectionSuccess,
@@ -9,23 +10,31 @@ import {
 const contentRoot = process.env.CONTENT_ROOT
 
 function* loadSection({ sectionId }) {
-  const loc = `${contentRoot}/de/${sectionId}/content.json`
-  try {
-    const res = yield fetch(loc)
-    if (res.status === 404) {
-      // it's ok if content.json is not there
-      yield put(loadSectionSuccess({
-        id: sectionId,
-        content: [],
-      }))
-    }
-    const data = yield res.json()
+  const cachedContent = yield select(selectors.getSectionContent, sectionId)
+  if (cachedContent) {
     yield put(loadSectionSuccess({
       id: sectionId,
-      content: data,
+      content: cachedContent,
     }))
-  } catch (err) {
-    yield put(loadSectionFailure(err))
+  } else {
+    const loc = `${contentRoot}/de/${sectionId}/content.json`
+    try {
+      const res = yield fetch(loc)
+      if (res.status === 404) {
+        // it's ok if content.json is not there
+        yield put(loadSectionSuccess({
+          id: sectionId,
+          content: [],
+        }))
+      }
+      const data = yield res.json()
+      yield put(loadSectionSuccess({
+        id: sectionId,
+        content: data,
+      }))
+    } catch (err) {
+      yield put(loadSectionFailure(err))
+    }
   }
 }
 
