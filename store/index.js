d332ebd38f35ba4993c0bf83b0ffef8d4ce1d0e8
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-05-24 12:50:09 +0200

    add hot reloading for sagas and reducers

--- store/index.js
+++ store/index.js
@@ -24,6 +24,23 @@ export default function configureStore(initialState = defaultInitialState) {
     bindMiddleware([sagaMiddleware])
   )
 
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('./reducers', () => {
+      // eslint-disable-next-line global-require
+      const nextReducer = require('./reducers').default
+      store.replaceReducer(nextReducer)
+    })
+    module.hot.accept('../sagas', () => {
+      // eslint-disable-next-line global-require
+      const nextSagas = require('../sagas').default
+      store.sagaTask.cancel()
+      store.sagaTask.done.then(() => {
+        store.runSagas(nextSagas)
+      })
+    })
+  }
+
   store.runSagaTask = () => {
     store.sagaTask = sagaMiddleware.run(rootSaga)
   }
