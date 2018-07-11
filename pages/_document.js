427ac5a8a5896ff3b9db5d7377f564ccbe4080a5
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-07-11 16:45:07 +0200

    upgrade npm packages

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class InnodocDocument extends Document {
         <Head>
           <title key="title">innoDoc web app</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
