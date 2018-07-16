15378588c2ab7eb60aed2d31898573629b2b4f67
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-07-16 17:06:47 +0200

    add jest and enzyme, write first test

---
+++ components/SectionLink.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Link from 'next/link'
+
+import SectionLink from './SectionLink'
+
+describe('<SectionLink />', () => {
+  it('renders', () => {
+    const wrapper = mount(
+      <SectionLink sectionId="87">
+        <a>
+          Hello World!
+        </a>
+      </SectionLink>
+    )
+    expect(wrapper.find(Link).exists()).toBe(true)
+    const a = wrapper.find('a')
+    expect(a.exists()).toBe(true)
+    expect(a.text()).toBe('Hello World!')
+    expect(a.prop('href')).toBe('/page/87')
+  })
+})
