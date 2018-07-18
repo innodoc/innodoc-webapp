74cd9bb6a19839ee617414234f0cddfd0ebb0907
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-07-18 14:32:34 +0200

    add test for store and initial state

---
+++ src/store/store.test.js
@@ -0,0 +1,54 @@
+import configureStore from './store'
+
+it('smoke test', () => {
+  let store
+  expect(() => {
+    store = configureStore()
+  }).not.toThrow()
+  expect(store).toBeDefined()
+})
+
+describe('initial state', () => {
+  const store = configureStore()
+  const state = store.getState()
+
+  describe('ui', () => {
+    const { ui } = state
+
+    test('hidden sidebar', () => {
+      expect(ui.sidebarVisible).toBe(false)
+    })
+
+    test('no message', () => {
+      expect(ui.message).toBe(null)
+    })
+  })
+
+  describe('content', () => {
+    const { content } = state
+
+    test('loading flag false', () => {
+      expect(content.loading).toBe(false)
+    })
+
+    test('current section null', () => {
+      expect(content.currentSectionId).toBe(null)
+    })
+
+    test('sections empty', () => {
+      expect(content.sections).toEqual({})
+    })
+
+    test('toc empty', () => {
+      expect(content.toc).toEqual([])
+    })
+  })
+
+  describe('exercises', () => {
+    const { exercises } = state
+
+    test('exercises empty', () => {
+      expect(exercises).toEqual({})
+    })
+  })
+})
