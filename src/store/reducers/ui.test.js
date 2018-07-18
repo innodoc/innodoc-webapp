520695931ecf315c9d8c15e3fb84087cb58d46fe
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-07-18 14:14:39 +0200

    add test for ui reducer

--- src/store/reducers/ui.test.js
+++ src/store/reducers/ui.test.js
@@ -1,4 +1,5 @@
-import { selectors } from './ui'
+import uiReducer, { selectors } from './ui'
+import { clearMessage, showMessage, toggleSidebar } from '../actions/ui'
 
 describe('ui selectors', () => {
   const state = {
@@ -21,6 +22,47 @@ describe('ui selectors', () => {
       title: 'Foo',
       msg: 'Bar',
       level: 'info',
+    })
+  })
+})
+
+describe('ui reducer', () => {
+  const initialState = {
+    message: null,
+    sidebarVisible: false,
+  }
+
+  const message = {
+    title: 'Foo',
+    msg: 'Text',
+    level: 'info',
+  }
+
+  it('should return the initial state', () => {
+    expect(uiReducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should handle SHOW_MESSAGE', () => {
+    expect(uiReducer(initialState, showMessage(message))).toEqual({
+      message,
+      sidebarVisible: false,
+    })
+  })
+
+  it('should handle CLEAR_MESSAGE', () => {
+    expect(uiReducer({
+      message,
+      sidebarVisible: false,
+    }, clearMessage())).toEqual({
+      message: null,
+      sidebarVisible: false,
+    })
+  })
+
+  it('should handle TOGGLE_SIDEBAR', () => {
+    expect(uiReducer(initialState, toggleSidebar())).toEqual({
+      message: null,
+      sidebarVisible: true,
     })
   })
 })
