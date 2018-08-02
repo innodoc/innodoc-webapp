bdb26e45401f42f254987f2aeeceacc59fe5cb90
Dennis Otrombowsky <dotrombowsky@outlook.de>
2018-08-02 18:08:50 +0200

    Added section buttons.

--- src/store/reducers/content.js
+++ src/store/reducers/content.js
@@ -4,6 +4,12 @@ import defaultInitialState, { defaultContentData } from '../defaultInitialState'
 
 const splitSectionId = id => id.split('/')
 
+const getSubSection = (currentSection, sectionId) => (
+  currentSection.children.find(s => s.id === sectionId)
+)
+const getSection = (root, idTokens) => idTokens.reduce(getSubSection, root)
+const getParentSection = (root, idTokens) => getSection(root, idTokens.slice(0, -1))
+
 export const selectors = {
   getContentLoading: state => state.content.loading,
   getCurrentSectionId: state => state.content.currentSectionId,
@@ -39,6 +45,73 @@ export const selectors = {
     })
 
     return result.sections
+  },
+  getPrevSectionId: (state, language, id) => {
+    if (id === null || id === undefined) {
+      return undefined
+    }
+
+    const root = { children: selectors.getToc(state, language) }
+    const idTokens = splitSectionId(id)
+    const currentSection = getSection(root, idTokens)
+    const parentSection = getParentSection(root, idTokens)
+    const currentSectionIndex = parentSection
+      .children
+      .findIndex(s => s.id === currentSection.id)
+
+    if (currentSectionIndex === 0) {
+      const prevSectionId = idTokens.slice(0, -1).join('/')
+      return prevSectionId === '' ? undefined : prevSectionId
+    }
+
+    const prevSectionIdTokens = idTokens.slice(0, -1)
+    prevSectionIdTokens
+      .push(parentSection.children[currentSectionIndex - 1].id)
+
+    // Look for last section
+    let current = getSection(root, prevSectionIdTokens)
+    while (current.children !== undefined) {
+      current = current.children[current.children.length - 1]
+      prevSectionIdTokens.push(current.id)
+    }
+
+    return prevSectionIdTokens.join('/')
+  },
+  getNextSectionId: (state, language, id, lvl) => { // Lvl should be undefined or 0 on initial call
+    if (id === null || id === undefined) {
+      return undefined
+    }
+
+    // Level indicates whether this is the initial call or a recursive one
+    const level = lvl === undefined ? 0 : lvl
+
+    const root = { children: selectors.getToc(state, language) }
+    const idTokens = splitSectionId(id)
+    const currentSection = getSection(root, idTokens)
+    const parentSection = getParentSection(root, idTokens)
+    const currentSectionIndex = parentSection
+      .children
+      .findIndex(s => s.id === currentSection.id)
+
+    if (
+      level === 0 // This must be the first call
+      && currentSection.children !== undefined
+      && currentSection.children.length > 0) {
+      idTokens.push(currentSection.children[0].id)
+      return idTokens.join('/')
+    }
+
+    if (currentSectionIndex === parentSection.children.length - 1) {
+      const parentSectionId = idTokens.slice(0, -1).join('/')
+      return parentSectionId === ''
+        ? undefined
+        : selectors.getNextSectionId(state, language, parentSectionId, level + 1)
+    }
+
+    const nextSectionId = idTokens.slice(0, -1)
+    nextSectionId
+      .push(parentSection.children[currentSectionIndex + 1].id)
+    return nextSectionId.join('/')
   },
 }
 
