935cfc752d7f93a20702d6703fd32481128c6bdb
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-08-27 14:44:20 +0200

    tmp

--- e2e/NightmareEnvironment.js
+++ e2e/NightmareEnvironment.js
@@ -1,15 +1,32 @@
 const NodeEnvironment = require('jest-environment-node')
 const nightmare = require('nightmare')
 const url = require('url')
+const waitOn = require('wait-on')
+
+const BASE_URL = url.format({
+  protocol: 'http',
+  hostname: 'localhost',
+  port: 7000,
+})
+
+const BASE_CONTENT_URL = url.format({
+  protocol: 'http',
+  hostname: 'localhost',
+  port: 7001,
+})
 
 class NightmareEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup()
 
-    const BASE_URL = url.format({
-      protocol: 'http',
-      hostname: 'localhost',
-      port: 7000,
+    waitOn({
+      resources: [BASE_URL, BASE_CONTENT_URL],
+      timeout: 30000,
+    }, (err) => {
+      if (err) {
+        throw err
+      }
+
     })
 
     this.global.visit = (path) => {
@@ -17,10 +34,23 @@ class NightmareEnvironment extends NodeEnvironment {
       this.browser = nightmare({
         show: process.env.DEBUG || false,
         pollInterval: 50,
+        gotoTimeout: 10000,
         loadTimeout: 15000,
+        waitTimeout: 15000,
       })
       return this.browser.goto(location)
     }
+  }
+
+  async teardown() {
+    this.browser.end().then(() => {
+      console.log('Nightmare test browser was killed.')
+    })
+    await super.teardown()
+  }
+
+  runScript(script) {
+    return super.runScript(script)
   }
 }
 
--- e2e/nightmareSmoke.test.js
+++ e2e/nightmareSmoke.test.js
@@ -2,14 +2,21 @@ describe('When visiting innoDoc webapp', () => {
   it('loads', async () => {
     const page = visit('/')
 
+    console.log('0')
+
     const title = await page.title()
     expect(title).toContain('innoDoc')
+
+    console.log('1')
 
     const menuItemExists = await page.exists('.header.item')
     expect(menuItemExists).toEqual(true)
 
+    console.log('2')
+
     const text = await page.evaluate(() => document.body.textContent).end()
     expect(text).toContain('Preparatory Mathematics')
+    console.log('3')
   })
 
   xit('should have a menu, footer and toc', () => {})
