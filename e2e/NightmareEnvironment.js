0bcc63e5338cf11326c29377ff447b3fe7ff7a71
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-09-04 15:36:10 +0200

    add E2E tests to CI

--- e2e/NightmareEnvironment.js
+++ e2e/NightmareEnvironment.js
@@ -1,33 +1,17 @@
-/* eslint-disable no-console */
-
-const http = require('http')
 const path = require('path')
-const { spawn } = require('child_process')
 const url = require('url')
+const Dotenv = require('dotenv-safe')
 const NodeEnvironment = require('jest-environment-node')
-const ecstatic = require('ecstatic')
 const nightmare = require('nightmare')
-const waitPort = require('wait-port')
 
-const PROD_PORT = 7000
-const CONTENT_PORT = 7001
+const rootDir = `${__dirname}/..`
+Dotenv.config({ path: path.normalize(`${rootDir}/.env`) })
 
 const BASE_URL = url.format({
   protocol: 'http',
   hostname: 'localhost',
-  port: PROD_PORT,
+  port: process.env.PROD_PORT,
 })
-
-const CONTENT_ROOT = url.format({
-  protocol: 'http',
-  hostname: 'localhost',
-  port: CONTENT_PORT,
-})
-
-const handleError = (err) => {
-  console.log(err)
-  throw err
-}
 
 const defaultHeaders = { 'Accept-Language': 'en' }
 
@@ -54,42 +38,6 @@ nightmare.action('getLangTag', function getLangTag(done) {
 class NightmareEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup()
-
-    // spawn app server
-    this.serverProcess = spawn('npm', ['run', 'start'], {
-      cwd: path.join(__dirname, '..'),
-      detached: true,
-      env: { PROD_PORT, CONTENT_ROOT },
-    })
-    this.serverProcess.on('error', handleError)
-    this.serverProcess.stdout.on('data', (data) => {
-      if (process.env.DEBUG) {
-        console.log(`'Server STDOUT': ${data.toString()}`)
-      }
-    })
-    this.serverProcess.stderr.on('data', (data) => {
-      throw new Error(`FATAL: Server is printing to STDERR: ${data.toString()}`)
-    })
-    if (!await waitPort({ timeout: 3000, output: 'silent', port: PROD_PORT })) {
-      throw new Error("Server didn't come up!")
-    }
-
-    // spawn content server
-    await new Promise((resolve, reject) => {
-      this.contentServer = http.createServer(ecstatic({
-        root: path.join(__dirname, 'content'),
-        cors: true,
-      })).listen(CONTENT_PORT, (err) => {
-        if (err) {
-          reject(err)
-        }
-        resolve()
-        if (process.env.DEBUG) {
-          console.log(`Started content server on port ${CONTENT_PORT}`)
-        }
-      })
-    })
-
     this.browsers = []
     // expose visit function to tests
     this.global.visit = (pathFragment, headers = {}, cookies = null) => {
@@ -105,14 +53,6 @@ class NightmareEnvironment extends NodeEnvironment {
   }
 
   async teardown() {
-    // stop app server process group
-    if (this.serverProcess.pid) {
-      process.kill(-this.serverProcess.pid)
-    }
-    // stop content server
-    if (this.contentServer.listening) {
-      this.contentServer.close()
-    }
     // make sure all browser instances are stopped
     await Promise.all(this.browsers.map(browser => browser.end()))
     await super.teardown()
