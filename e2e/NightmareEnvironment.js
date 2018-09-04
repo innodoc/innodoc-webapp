16962844122c97a48f6d84bf04e23fb33484b413
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-09-04 15:36:10 +0200

    add E2E tests to CI

--- e2e/NightmareEnvironment.js
+++ e2e/NightmareEnvironment.js
@@ -1,45 +1,26 @@
-/* eslint-disable no-console */
-
-const http = require('http')
 const path = require('path')
-const { spawn } = require('child_process')
 const url = require('url')
+const Dotenv = require('dotenv-safe')
 const NodeEnvironment = require('jest-environment-node')
-const ecstatic = require('ecstatic')
 const nightmare = require('nightmare')
-const waitPort = require('wait-port')
 
-const PROD_PORT = 7000
-const CONTENT_PORT = 7001
+const rootDir = `${__dirname}/..`
+Dotenv.config({ path: path.normalize(`${rootDir}/.env`) })
 
 const BASE_URL = url.format({
   protocol: 'http',
   hostname: 'localhost',
-  port: PROD_PORT,
+  port: process.env.PROD_PORT,
 })
-
-const CONTENT_ROOT = url.format({
-  protocol: 'http',
-  hostname: 'localhost',
-  port: CONTENT_PORT,
-})
-
-const handleError = (err) => {
-  console.log(err)
-  throw err
-}
 
 const defaultHeaders = { 'Accept-Language': 'en' }
 
 const nightmareOptions = {
   show: process.env.NIGHTMARE_SHOW || false,
-  gotoTimeout: 2000,
-  loadTimeout: 2000,
-  waitTimeout: 2000,
-  executionTimeout: 2000,
+  loadTimeout: 30000,
 }
 
-// Nightmare actions
+// custom nightmare actions
 nightmare.action('extractText', function extractText(selectorArg, done) {
   this.evaluate_now((selector) => {
     const result = document.querySelector(selector)
@@ -52,46 +33,14 @@ nightmare.action('getLangTag', function getLangTag(done) {
 
 // Jest test environment for Nightmare
 class NightmareEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config)
+    this.browsers = [] // remember open browser instances
+  }
+
   async setup() {
     await super.setup()
-
-    // spawn app server
-    this.serverProcess = spawn('npm', ['run', 'start'], {
-      cwd: path.join(__dirname, '..'),
-      detached: true,
-      env: { PROD_PORT, CONTENT_ROOT },
-    })
-    this.serverProcess.on('error', handleError)
-    this.serverProcess.stdout.on('data', (data) => {
-      if (process.env.DEBUG) {
-        console.log(`'Server STDOUT': ${data.toString()}`)
-      }
-    })
-    this.serverProcess.stderr.on('data', (data) => {
-      throw new Error(`FATAL: Server is printing to STDERR: ${data.toString()}`)
-    })
-    if (!await waitPort({ timeout: 3000, output: 'silent', port: PROD_PORT })) {
-      throw new Error("Server didn't come up!")
-    }
-
-    // spawn content server
-    await new Promise((resolve, reject) => {
-      this.contentServer = http.createServer(ecstatic({
-        root: path.join(__dirname, 'content'),
-        cors: true,
-      })).listen(CONTENT_PORT, (err) => {
-        if (err) {
-          reject(err)
-        }
-        resolve()
-        if (process.env.DEBUG) {
-          console.log(`Started content server on port ${CONTENT_PORT}`)
-        }
-      })
-    })
-
-    this.browsers = []
-    // expose visit function to tests
+    // visit spawns a browser and visits the given path
     this.global.visit = (pathFragment, headers = {}, cookies = null) => {
       const location = url.resolve(BASE_URL, pathFragment)
       const browser = nightmare(nightmareOptions)
@@ -104,16 +53,8 @@ class NightmareEnvironment extends NodeEnvironment {
     }
   }
 
+  // make sure all browser instances are stopped on exit
   async teardown() {
-    // stop app server process group
-    if (this.serverProcess.pid) {
-      process.kill(-this.serverProcess.pid)
-    }
-    // stop content server
-    if (this.contentServer.listening) {
-      this.contentServer.close()
-    }
-    // make sure all browser instances are stopped
     await Promise.all(this.browsers.map(browser => browser.end()))
     await super.teardown()
   }
---
+++ e2e/ci-e2e.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+export NIGHTMARE_SHOW
+export PROD_PORT=7000
+export CONTENT_PORT=7001
+export CONTENT_ROOT="http://localhost:${CONTENT_PORT}/"
+
+# start app and content server
+npm run test:e2e:content 2>&1 >/dev/null &
+pid_content=$!
+npm run start 2>&1 >/dev/null &
+pid_app=$!
+
+# wait for app and content server
+while ! nc -z localhost $CONTENT_PORT; do sleep 0.1; done
+while ! nc -z localhost $PROD_PORT; do sleep 0.1; done
+
+# check for DISPLAY
+cmd="npm run test:e2e"
+if [[ ! $DISPLAY ]]; then
+  echo "Running in headless mode (using Xvfb)"
+  cmd="xvfb-run --auto-servernum $cmd"
+fi
+
+# run tests
+$cmd
+
+# kill app and content server
+kill -INT $(lsof -ti tcp:${CONTENT_PORT})
+wait $pid_content
+kill -INT $(lsof -ti tcp:${PROD_PORT})
+wait $pid_app
+
+exit $?
