4ee055a8692ac4994c7cb017f67dfcdfe10b6a4d
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-09-05 15:00:08 +0200

    tmp

--- e2e/ci-e2e.sh
+++ e2e/ci-e2e.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-trap "kill 0" EXIT
-set -e
+function finish {
+  trap '' INT TERM # ignore INT and TERM while shutting down
+  kill -TERM 0 # kill process group
+  wait
+}
+trap finish EXIT
 
 export NIGHTMARE_SHOW
 export PROD_PORT=7000
-export CONTENT_PORT=7002
+export CONTENT_PORT=7001
 export CONTENT_ROOT="http://localhost:${CONTENT_PORT}/"
 
 # start app and content server
@@ -16,14 +20,12 @@ npm run start >/dev/null &
 while ! nc -z localhost $CONTENT_PORT; do sleep 0.1; done
 while ! nc -z localhost $PROD_PORT; do sleep 0.1; done
 
-# e2e test command
-cmd="npm run test:e2e"
-
 # check for display
-if [[ $DISPLAY ]]; then
-  $cmd
-else
+cmd="npm run test:e2e"
+if [[ ! $DISPLAY ]]; then
   echo "Running in headless mode (using Xvfb)"
-  # headless mode (CI)
-  xvfb-run --server-args="-screen 0 1024x768x24" $cmd
+  cmd="xvfb-run --server-args="-screen 0 1920x1080x24" $cmd"
 fi
+
+$cmd
+exit $?
