42f48a9dfb59aa0165983e471ac4bce39daffcad
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-09-05 14:04:03 +0200

    tmp

--- e2e/ci-e2e.sh
+++ e2e/ci-e2e.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-trap "kill 0" EXIT
-set -e
+function finish {
+  trap '' INT TERM # ignore INT and TERM while shutting down
+  kill -TERM 0 # kill process group
+  wait
+}
+trap finish EXIT
 
 export NIGHTMARE_SHOW
-export PROD_PORT=7000
+export PROD_PORT=7001
 export CONTENT_PORT=7002
 export CONTENT_ROOT="http://localhost:${CONTENT_PORT}/"
 
 # start app and content server
-npm run test:e2e:content >/dev/null &
-npm run start >/dev/null &
+npm run test:e2e:content &
+pid_content=$!
+npm run start &
+pid_app=$!
 
 # wait for app and content server
 while ! nc -z localhost $CONTENT_PORT; do sleep 0.1; done
 while ! nc -z localhost $PROD_PORT; do sleep 0.1; done
 
-# e2e test command
-cmd="npm run test:e2e"
-
-# check for display
-if [[ $DISPLAY ]]; then
-  $cmd
-else
-  echo "Running in headless mode (using Xvfb)"
-  # headless mode (CI)
-  xvfb-run --server-args="-screen 0 1024x768x24" $cmd
+if [[ ! $DISPLAY ]]; then
+  echo "Running in headless mode (using Xdummy)"
+  /usr/bin/Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config ${PWD}/e2e/xorg-dummy.conf :99 &
+  pid_xorg=$!
+  export DISPLAY=:99
 fi
+
+npm run test:e2e
+exit $?
