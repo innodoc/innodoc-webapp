145ff0f9fad6d329952e17bb004a31eeecb69a62
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-09-12 21:48:59 +0200

    add tests for sagas/content/changeLanguage

---
+++ src/sagas/content/changeLanguage.test.js
@@ -0,0 +1,25 @@
+import { put, select } from 'redux-saga/effects'
+import { cloneableGenerator } from 'redux-saga/utils'
+
+import changeLanguage from './changeLanguage'
+import { loadToc, loadSection } from '../../store/actions/content'
+import selectors from '../../store/selectors/content'
+
+describe('changeLanguage', () => {
+  const gen = cloneableGenerator(changeLanguage)()
+
+  it('should reload content', () => {
+    const clone = gen.clone()
+    expect(clone.next().value).toEqual(put(loadToc()))
+    expect(clone.next().value).toEqual(select(selectors.getCurrentSectionPath))
+    expect(clone.next('/path/').value).toEqual(put(loadSection('/path/')))
+    expect(clone.next().done).toEqual(true)
+  })
+
+  it('should reload toc but not section without section path', () => {
+    const clone = gen.clone()
+    expect(clone.next().value).toEqual(put(loadToc()))
+    expect(clone.next().value).toEqual(select(selectors.getCurrentSectionPath))
+    expect(clone.next().done).toEqual(true)
+  })
+})
