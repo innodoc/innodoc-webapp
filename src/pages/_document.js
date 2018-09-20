51a8143b261e0ce068dceb7619bc4ed6fd08004b
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-09-20 16:54:50 +0200

    remove superflous style.css

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,7 +14,6 @@ class InnodocDocument extends Document {
           </title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
-          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
           <Main />
