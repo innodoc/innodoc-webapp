9006f34c2259fe94d27173bfcaedafa4f877e55f
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-10-25 18:37:36 +0200

    add tests for Placeholder

--- src/components/hoc/withLoadingPlaceholder/withLoadingPlaceholder.test.js
+++ src/components/hoc/withLoadingPlaceholder/withLoadingPlaceholder.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { Dimmer, Loader } from 'semantic-ui-react'
 
 import withLoadingPlaceholder from './withLoadingPlaceholder'
 
@@ -19,16 +18,12 @@ describe('<withLoadingPlaceholder />', () => {
 
   it('renders placeholder while loading', () => {
     const wrapper = shallow(<WrapperComponent loading />)
-    expect(wrapper.find(Dimmer)).toHaveLength(1)
-    expect(wrapper.find(Loader)).toHaveLength(1)
     expect(wrapper.find(MockPlaceholder)).toHaveLength(1)
     expect(wrapper.find(MockComponent)).toHaveLength(0)
   })
 
   it('renders component if not loading', () => {
     const wrapper = shallow(<WrapperComponent loading={false} someOtherProp={42} />)
-    expect(wrapper.find(Dimmer)).toHaveLength(1)
-    expect(wrapper.find(Loader)).toHaveLength(1)
     expect(wrapper.find(MockPlaceholder)).toHaveLength(0)
     expect(wrapper.find(MockComponent)).toHaveLength(1)
     expect(wrapper.find(MockComponent).prop('someOtherProp')).toEqual(42)
