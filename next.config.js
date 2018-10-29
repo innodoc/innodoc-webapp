68867c5c557762b51c61cb21846eb9939710cb3b
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-10-29 19:29:45 +0100

    add AntdScssThemePlugin

--- next.config.js
+++ next.config.js
@@ -1,9 +1,59 @@
+const path = require('path')
 const cssLoaderConfig = require('@zeit/next-css/css-loader-config')
-const withSass = require('@zeit/next-sass')
 const Dotenv = require('dotenv-webpack')
+const AntdScssThemePlugin = require('antd-scss-theme-plugin')
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
-// modified version of @zeit/next-less without CSS modules
+// modified version of @zeit/next-sass that adds AntdScssThemePlugin
+const withSassWithAntdScss = (nextConfig = {}) => ({
+  ...nextConfig,
+  webpack(config, options) {
+    const { dev, isServer } = options
+    const {
+      cssModules,
+      cssLoaderOptions,
+      postcssLoaderOptions,
+      sassLoaderOptions = {},
+    } = nextConfig
+
+    /* eslint-disable-next-line no-param-reassign */
+    options.defaultLoaders.sass = cssLoaderConfig(config, {
+      extensions: ['scss', 'sass'],
+      cssModules,
+      cssLoaderOptions,
+      postcssLoaderOptions,
+      dev,
+      isServer,
+      loaders: [
+        AntdScssThemePlugin.themify({
+          loader: 'sass-loader',
+          options: sassLoaderOptions,
+        }),
+      ],
+    })
+
+    config.module.rules.push(
+      {
+        test: /\.scss$/,
+        use: options.defaultLoaders.sass,
+      },
+      {
+        test: /\.sass$/,
+        use: options.defaultLoaders.sass,
+      }
+    )
+
+    if (typeof nextConfig.webpack === 'function') {
+      return nextConfig.webpack(config, options)
+    }
+
+    return config
+  },
+})
+
+// modified version of @zeit/next-less
+// - withSassWithAntdScss
+// - disable without CSS modules
 const withLessWithoutCssModules = (nextConfig = {}) => ({
   ...nextConfig,
   webpack(config, options) {
@@ -26,10 +76,10 @@ const withLessWithoutCssModules = (nextConfig = {}) => ({
       dev,
       isServer,
       loaders: [
-        {
+        AntdScssThemePlugin.themify({
           loader: 'less-loader',
           options: lessLoaderOptions,
-        },
+        }),
       ],
     })
 
@@ -46,7 +96,7 @@ const withLessWithoutCssModules = (nextConfig = {}) => ({
   },
 })
 
-module.exports = withBundleAnalyzer(withLessWithoutCssModules(withSass({
+module.exports = withBundleAnalyzer(withLessWithoutCssModules(withSassWithAntdScss({
   // only use .js (not .jsx)
   pageExtensions: ['js'],
 
@@ -78,10 +128,12 @@ module.exports = withBundleAnalyzer(withLessWithoutCssModules(withSass({
   // webpack
   webpack: (config) => {
     config.plugins.push(new Dotenv({
-      path: `${__dirname}/.env`,
+      path: path.join(__dirname, '.env'),
       safe: true,
       systemvars: true,
     }))
+
+    config.plugins.push(new AntdScssThemePlugin(path.join(__dirname, './src/antd-theme.sass')))
 
     // debug print webpack config
     /* eslint-disable-next-line no-extend-native */
