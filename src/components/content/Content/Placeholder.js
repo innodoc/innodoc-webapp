92f8d33fbf619fa1fa20f3569c1a4f846bfe2234
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-11-01 12:25:14 +0100

    add antd Placeholder

--- src/components/content/Content/Placeholder.js
+++ src/components/content/Content/Placeholder.js
@@ -1,39 +1,9 @@
 import React from 'react'
-import { Header, Placeholder } from 'semantic-ui-react'
-
-import BreadcrumbPlaceholder from '../Breadcrumb/Placeholder'
-import css from './style.sass'
+import Skeleton from 'antd/lib/skeleton'
 
 const ContentPlaceholder = () => (
   <React.Fragment>
-    <BreadcrumbPlaceholder />
-    <Header>
-      <Placeholder fluid>
-        <Placeholder.Header>
-          <Placeholder.Line />
-        </Placeholder.Header>
-      </Placeholder>
-    </Header>
-    <div className={css.content}>
-      <Placeholder fluid>
-        <Placeholder.Paragraph>
-          <Placeholder.Line />
-          <Placeholder.Line />
-          <Placeholder.Line />
-          <Placeholder.Line />
-        </Placeholder.Paragraph>
-      </Placeholder>
-      <Placeholder style={{ height: 200, width: 250 }}>
-        <Placeholder.Image />
-      </Placeholder>
-      <Placeholder fluid>
-        <Placeholder.Paragraph>
-          <Placeholder.Line />
-          <Placeholder.Line />
-          <Placeholder.Line />
-        </Placeholder.Paragraph>
-      </Placeholder>
-    </div>
+    <Skeleton active />
   </React.Fragment>
 )
 
