82b3b484b6c71e0e611e6d9b959a35d72f58cc36
Mirko Dietrich <dietrich@math.tu-berlin.de>
2018-12-20 18:56:33 +0100

    add e2e tests for error conditions

--- e2e/errorConditions.test.js
+++ e2e/errorConditions.test.js
@@ -1,6 +1,15 @@
 describe('Error conditions', () => {
-  const page = visit('/page/does-not-exist')
+  test.each(['/path-does-not-exist', '/page', '/page/', '/page/does-not-exist'])(
+    'receives 404 for URL: %s', async (url) => {
+      const page = visit(url)
+      expect((await page).code).toEqual(404)
+    }
+  )
 
-  it('loads without error', async () => {
+  it('should show "Not found" also client-side', async () => {
+    const page = visit('/page/02-elements/03-links-and-formatting')
+    page.click('[href*=does-not-exist]')
+    const modalText = await page.extractText('.ant-alert')
+    expect(modalText).toContain('404')
   })
 })
