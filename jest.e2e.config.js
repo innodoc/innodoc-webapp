8c3bca1a73ca94147f0eb883d258c0ac232f2ee7
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-02-07 12:25:14 +0100

    fix e2e tests

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,5 +2,5 @@ module.exports = {
   roots: ['e2e'],
   verbose: true,
   testEnvironment: './e2e/NightmareEnvironment.js',
-  setupFilesAfterEnv: ['<rootDir>/e2e/jest.setup.js'],
+  setupFiles: ['<rootDir>/e2e/jest.setup.js'],
 }
