6e4266483fe6c63c944966acab3fdf2e95c44710
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-02-12 16:12:36 +0100

    tmp

--- e2e/smoke.test.js
+++ e2e/smoke.test.js
@@ -2,11 +2,13 @@ describe('Initial smoke test', () => {
   const page = visit('/')
 
   it('loads without error', async () => {
+    expect.assertions(1)
     const modalText = await page.extractText('.modal')
     expect(modalText.toLowerCase()).not.toContain('error')
   })
 
   it('should have a menu, footer and toc', async () => {
+    expect.assertions(5)
     const title = await page.title()
     expect(title).toContain('innoDoc')
     expect(await page.exists('.ant-menu')).toEqual(true)
