1fea121b080fe3ab54048711847236c14bde99bc
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-02-12 15:28:47 +0100

    tmp

--- e2e/errorConditions.test.js
+++ e2e/errorConditions.test.js
@@ -1,12 +1,12 @@
 describe('Error conditions', () => {
   test.each(['/path-does-not-exist', '/page', '/page/', '/page/does-not-exist'])(
     'receives 404 for URL: %s', async (url) => {
-      const page = visit(url)
-      expect((await page).code).toEqual(404)
+      const page = await visit(url)
+      expect(page.code).toEqual(404)
     }
   )
 
-  it('should show "Not found" also client-side', async () => {
+  it('should show "404" also client-side', async () => {
     const page = visit('/page/02-elements/03-links-and-formatting')
     page.click('[href*=does-not-exist]')
     const modalText = await page.extractText('.ant-alert')
