251e2499e53f33ff4e5b6bd1ff592c284f020ba4
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-02-13 17:52:27 +0100

    tmp

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,13 +2,20 @@ const headless = process.env.PUPPETEER_HEADLESS !== 'true'
 const launchTimeout = 15000
 const protocol = 'http'
 const usedPortAction = 'error'
+const launch = {
+  headless,
+  slowMo: headless ? 0 : 250,
+}
+
+// TODO: detect alpine (CI)
+console.log('HHIAAAAA')
+console.log(`process.env.CHROME_BIN ${process.env.CHROME_BIN}`)
+console.log(`process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD ${process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD}`)
+launch.args = ['--no-sandbox', '--disable-dev-shm-usage']
+launch.executablePath = '/usr/bin/chromium-browser'
 
 module.exports = {
-  launch: {
-    headless,
-    slowMo: headless ? 0 : 250,
-    args: ['--disable-dev-shm-usage'],
-  },
+  launch,
   server: [
     {
       command: `PROD_PORT=${process.env.PROD_PORT} npm run start`,
