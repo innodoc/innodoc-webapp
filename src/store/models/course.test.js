268efb7b7151a856216feb08e5cf3db911cf7bc8
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-04-08 17:41:55 +0200

    fix src/store/models/course.test.js

--- src/store/models/course.test.js
+++ src/store/models/course.test.js
@@ -4,79 +4,93 @@ import { loadManifestSuccess, loadSection, loadSectionSuccess } from '../actions
 
 const createEmptyState = () => {
   const state = orm.getEmptyState()
-  const session = orm.mutableSession(state)
+  const session = orm.session(state)
   const { App } = session
   App.create({ id: 0, language: 'en' })
   return session
 }
 
-const manifest = {
-  homeLink: 'bar',
-  languages: ['en'],
-  title: { en: 'foobar' },
-  toc: [
-    {
-      id: 'foo',
-      title: { en: 'foo' },
-    },
-    {
-      id: 'bar',
-      title: { en: 'bar' },
-    },
-  ],
-}
+let session
+
+beforeEach(() => {
+  session = createEmptyState()
+})
 
 describe('model', () => {
   test('create', () => {
-    const session = createEmptyState()
     session.Course.create({})
     expect(session.Course.all().count()).toEqual(1)
   })
 })
 
 describe('reducer', () => {
-  test('loadManifestSuccess', () => {
-    const session = createEmptyState()
-    CourseModel.reducer(
-      loadManifestSuccess({ content: manifest }), session.Course, session)
-    expect(session.Course.first().ref).toEqual({
-      ...manifest,
-      id: 0,
-      currentSection: null,
-      toc: undefined,
+  describe('loadManifest', () => {
+    const manifest = {
+      homeLink: 'bar',
+      languages: ['en'],
+      title: { en: 'foobar' },
+      toc: [
+        { id: 'foo', title: { en: 'foo' } },
+        { id: 'bar', title: { en: 'bar' } },
+      ],
+    }
+
+    test('loadManifestSuccess', () => {
+      CourseModel.reducer(
+        loadManifestSuccess({ content: manifest }), session.Course, session)
+      expect(session.Course.first().ref).toEqual({
+        ...manifest,
+        id: 0,
+        currentSection: null,
+        toc: undefined,
+      })
+    })
+
+    test('loadManifestSuccess (w/o homeLink)', () => {
+      const action = loadManifestSuccess({
+        content: { ...manifest, homeLink: null },
+      })
+      CourseModel.reducer(action, session.Course, session)
+      expect(session.Course.first().ref).toEqual({
+        ...manifest,
+        homeLink: 'foo',
+        id: 0,
+        currentSection: null,
+        toc: undefined,
+      })
     })
   })
 
-  test('loadManifestSuccess (w/o homeLink)', () => {
-    const session = createEmptyState()
-    const action = loadManifestSuccess({
-      content: { ...manifest, homeLink: null },
-    })
-    CourseModel.reducer(action, session.Course, session)
-    expect(session.Course.first().ref).toEqual({
-      ...manifest,
-      homeLink: 'foo',
-      id: 0,
-      currentSection: null,
-      toc: undefined,
-    })
-  })
+  describe('loadSection', () => {
+    let course
+    let sectionFoo
+    let sectionBar
 
-  test('loadSection', () => {
-    const session = createEmptyState()
-    CourseModel.reducer(loadManifestSuccess({ content: manifest }), session.Course, session)
-    session.App.first().set('currentCourse', 0)
-    const course = session.Course.first()
-    CourseModel.reducer(loadSection('/path/'), session.Course, session)
-    expect(course.ref.currentSection).toEqual(null)
-  })
+    beforeEach(() => {
+      sectionFoo = session.Section.create({
+        id: '/foo/',
+        title: 'Foo',
+      })
+      sectionBar = session.Section.create({
+        id: '/bar/',
+        title: 'Bar',
+      })
+      course = session.Course.create({
+        currentSection: sectionFoo,
+        homeLink: '/foo/',
+        languages: ['en'],
+        title: 'Foo course',
+      })
+    })
 
-  test('loadSectionSuccess', () => {
-    const session = createEmptyState()
-    CourseModel.reducer(loadManifestSuccess({ content: manifest }), session.Course, session)
-    session.App.first().set('currentCourse', 0)
-    const course = session.Course.first()
-    CourseModel.reducer(loadSectionSuccess({ sectionId: '/path/' }), session.Course, session)
-    expect(course.ref.currentSection).toEqual('/path/')
+    test('loadSection', () => {
+      CourseModel.reducer(loadSection('/bar/'), session.Course, session)
+      expect(course.ref.currentSection).toEqual(null)
+    })
+
+    test('loadSectionSuccess', () => {
+      CourseModel.reducer(loadSectionSuccess({ sectionId: '/bar/' }), session.Course, session)
+      expect(course.ref.currentSection).toEqual(sectionBar.ref.id)
+    })
   })
 })
