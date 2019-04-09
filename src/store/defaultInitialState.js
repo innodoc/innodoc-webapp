4da8af68a040f095a5a27919b9d2c6898aaa5bb7
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-04-09 15:52:24 +0200

    add TODO

--- src/store/defaultInitialState.js
+++ src/store/defaultInitialState.js
@@ -2,6 +2,7 @@ import orm from './orm'
 
 const defaultInitialState = () => {
   const ormState = orm.getEmptyState()
+  // TODO: better to create app object somewhere in a reducer?
   const session = orm.mutableSession(ormState)
   session.App.create({
     currentCourse: null,
