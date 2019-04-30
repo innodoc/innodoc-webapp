224c24989412bb63f921c156ef37f89af0d3a6a5
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-04-30 16:49:03 +0200

    add question saga tests

--- src/sagas/question/question.js
+++ src/sagas/question/question.js
@@ -3,16 +3,20 @@ import { call, put, throttle } from 'redux-saga/effects'
 import { actionTypes as questionActionTypes, questionSolved } from '../../store/actions/question'
 import checkers from '../../lib/questionCheckers'
 
+export const QUESTION_ANSWER_THROTTLE = 500
+
 export function* handleQuestionAnswered({ data }) {
   const { questionId, attributes, answer } = data
-  const { validation, solution } = attributes
-  const checker = checkers[validation]
+  const { questionType, solution, ...remainingAttrs } = attributes
+  const checker = checkers[questionType]
   if (checker) {
-    const correct = yield call(checker, answer, solution, attributes)
+    const correct = yield call(checker, answer, solution, remainingAttrs)
     yield put(questionSolved({ questionId, answer, correct }))
   }
 }
 
 export default function* watchQuestionChange() {
-  yield throttle(500, questionActionTypes.QUESTION_ANSWERED, handleQuestionAnswered)
+  yield throttle(
+    QUESTION_ANSWER_THROTTLE, questionActionTypes.QUESTION_ANSWERED, handleQuestionAnswered
+  )
 }
