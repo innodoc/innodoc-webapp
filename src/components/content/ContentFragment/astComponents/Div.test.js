887dc858dce63747992d178c0a587b83f8bb2ce3
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-04-30 17:01:45 +0200

    fix test for Div

--- src/components/content/ContentFragment/astComponents/Div.test.js
+++ src/components/content/ContentFragment/astComponents/Div.test.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import ExerciseCard from '../cards/ExerciseCard'
-import Figure from './Figure'
 import Div from './Div'
 import UnknownType from './UnknownType'
+import ExerciseCard from '../cards/ExerciseCard'
+import Figure from './Figure'
 
 describe('<Div />', () => {
-  it.each([['figure', Figure], ['exercise', ExerciseCard], ['does-not-exist']])(
+  it.each([['figure', Figure], ['exercise', ExerciseCard]])(
     'should map className "%s" to correct Component', (className, Component) => {
-      const wrapper = shallow(<Div data={[[null, [className]], []]} />)
+      const content = [{ foo: 'foo' }, { bar: 'bar' }]
+      const wrapper = shallow(<Div data={[[null, [className]], content]} />)
       expect(wrapper.is(Component)).toBe(true)
+      expect(wrapper.prop('content')).toBe(content)
     })
 
   it('should render UnknownType for an unknown className', () => {
