1f545573614c8ea08be82ed6a574b1215d115ecd
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-04-30 16:49:03 +0200

    add question saga tests

--- src/sagas/question/question.test.js
+++ src/sagas/question/question.test.js
@@ -1,60 +1,49 @@
-import {
-  call,
-  fork,
-  put,
-  take,
-} from 'redux-saga/effects'
+import { expectSaga, testSaga } from 'redux-saga-test-plan'
 
-import watchQuestionChange, { handleQuestionAnswered } from './question'
-import { actionTypes as questionActionTypes, questionAnswered } from '../../store/actions/question'
+import watchQuestionChange, { handleQuestionAnswered, QUESTION_ANSWER_THROTTLE } from './question'
+import { actionTypes as questionActionTypes, questionSolved } from '../../store/actions/question'
 import checkers from '../../lib/questionCheckers'
 
-const wrongAnswer = questionAnswered({
-  attrs: { questionType: 'exact' },
-  id: 'testExercise',
-  inputValue: 'tes',
-  solved: false,
-  solution: 'test',
-})
-const correctAnswer = questionAnswered({
-  attrs: { questionType: 'exact' },
-  id: 'testExercise',
-  inputValue: 'test',
-  solved: false,
-  solution: 'test',
-})
-
 describe('handleQuestionAnswered', () => {
-  test('wrong input', () => {
-    const gen = handleQuestionAnswered(wrongAnswer)
-    expect(gen.next().value).toEqual(call(
-      checkers[wrongAnswer.data.attrs.questionType],
-      wrongAnswer.data.inputValue,
-      wrongAnswer.data.solution,
-      wrongAnswer.data.attrs))
-    expect(gen.next(false).done).toEqual(true)
+  it('should call the checker and put questionSolved', () => {
+    const data = {
+      questionId: 'foo/bar#Q1',
+      answer: '41',
+      attributes: {
+        questionType: 'exact',
+        solution: '42',
+      },
+    }
+    return expectSaga(handleQuestionAnswered, { data })
+      .call.like({ fn: checkers.exact })
+      .put(questionSolved({ questionId: 'foo/bar#Q1', answer: '41', correct: false }))
+      .run()
   })
 
-  test('right input', () => {
-    const gen = handleQuestionAnswered(correctAnswer)
-    expect(gen.next().value).toEqual(call(
-      checkers[correctAnswer.data.attrs.questionType],
-      correctAnswer.data.inputValue,
-      correctAnswer.data.solution,
-      correctAnswer.data.attrs))
-    expect(gen.next().value).toEqual(put(questionAnswered({
-      solved: true,
-      ...correctAnswer.data,
-    })))
-    expect(gen.next().done).toEqual(true)
+  it('should not put questionSolved with unknown checker', () => {
+    const data = {
+      questionId: 'foo/bar#Q1',
+      answer: '41',
+      attributes: {
+        questionType: 'this-checker-does-not-exist',
+        solution: '42',
+      },
+    }
+    return expectSaga(handleQuestionAnswered, { data })
+      .not.put.actionType(questionActionTypes.QUESTION_SOLVED)
+      .run()
   })
 })
 
 describe('watchQuestionChange', () => {
-  test('exercise input change', () => {
-    const gen = watchQuestionChange()
-    expect(gen.next().value).toEqual(take(questionActionTypes.QUESTION_ANSWERED))
-    expect(gen.next(wrongAnswer).value)
-      .toEqual(fork(handleQuestionAnswered, wrongAnswer))
+  it('should throttle handleQuestionAnswered saga on QUESTION_ANSWERED', () => {
+    testSaga(watchQuestionChange)
+      .next()
+      .throttle(
+        QUESTION_ANSWER_THROTTLE,
+        questionActionTypes.QUESTION_ANSWERED,
+        handleQuestionAnswered)
+      .next()
+      .isDone()
   })
 })
