5cf336ab09e28ca76c44dabd371b3c0e19dbd970
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-05-02 12:49:56 +0200

    add tests for ExerciseCard and Co.

--- src/components/content/ContentFragment/cards/ExerciseCard/ExerciseContext.test.js
+++ src/components/content/ContentFragment/cards/ExerciseCard/ExerciseContext.test.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { shallow } from 'enzyme'
 
+import ExerciseContext from './ExerciseContext'
+
 describe('<ExerciseContext />', () => {
-  xit('renders', () => {})
+  it('should provide defaults', () => {
+    const MyComponent = () => {
+      const exerciseContext = useContext(ExerciseContext)
+      return <div {...exerciseContext} />
+    }
+    const div = shallow(<MyComponent />).find('div')
+    expect(div.prop('addQuestion')).toBeInstanceOf(Function)
+    expect(div.prop('addQuestionAnswered')).toBeInstanceOf(Function)
+    expect(div.prop('allAnswered')).toBeInstanceOf(Function)
+    expect(div.prop('getShowResult')).toBeInstanceOf(Function)
+    expect(div.prop('setAutoVerify')).toBeInstanceOf(Function)
+    expect(div.prop('setUserTriggeredVerify')).toBeInstanceOf(Function)
+    expect(typeof div.prop('userTriggeredVerify')).toBe('boolean')
+  })
 })
--- src/components/content/ContentFragment/cards/ExerciseCard/ExerciseProvider.test.js
+++ src/components/content/ContentFragment/cards/ExerciseCard/ExerciseProvider.test.js
@@ -1,6 +1,50 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
+import ExerciseContext from './ExerciseContext'
+import ExerciseProvider from './ExerciseProvider'
+
+const createContext = () => {
+  const wrapper = shallow(
+    <ExerciseProvider>
+      <div />
+    </ExerciseProvider>
+  )
+  return () => wrapper.find(ExerciseContext.Provider).prop('value')
+}
+
 describe('<ExerciseProvider />', () => {
-  xit('renders', () => {})
+  describe('question tracking', () => {
+    it('detects all answered', () => {
+      const getContext = createContext()
+      getContext().addQuestion('foo/bar#EX0')
+      getContext().addQuestionAnswered('foo/bar#EX0')
+      getContext().addQuestion('foo/bar#EX1')
+      getContext().addQuestionAnswered('foo/bar#EX1')
+      expect(getContext().allAnswered()).toBe(true)
+    })
+
+    it('detects not all answered', () => {
+      const getContext = createContext()
+      getContext().addQuestion('foo/bar#EX0')
+      getContext().addQuestionAnswered('foo/bar#EX0')
+      getContext().addQuestion('foo/bar#EX1')
+      expect(getContext().allAnswered()).toBe(false)
+    })
+  })
+
+  describe('show results', () => {
+    it('should always show results (default)', () => {
+      const getContext = createContext()
+      expect(getContext().getShowResult()).toBe(true)
+    })
+
+    it('should show results only when triggered (autoVerify=false)', () => {
+      const getContext = createContext()
+      getContext().setAutoVerify(false)
+      expect(getContext().getShowResult()).toBe(false)
+      getContext().setUserTriggeredVerify(true)
+      expect(getContext().getShowResult()).toBe(true)
+    })
+  })
 })
