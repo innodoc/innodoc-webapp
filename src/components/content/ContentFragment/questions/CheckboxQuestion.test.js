9c2a02ea9812847110e6ab4a43088ed2ea29ba0f
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-05-06 18:20:41 +0200

    add CheckboxQuestion test

---
+++ src/components/content/ContentFragment/questions/CheckboxQuestion.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Checkbox from 'antd/lib/checkbox'
+import Icon from 'antd/lib/icon'
+
+import CheckboxQuestion from './CheckboxQuestion'
+
+describe('<CheckboxQuestion />', () => {
+  it.each([
+    ['0', false, false],
+    ['1', true, false],
+    [null, undefined, true],
+  ])('should render: value=%s, checkbox(checked=%s, indeterminate=%s)', (value, checked, indeterminate) => {
+    const onChange = jest.fn()
+    const wrapper = shallow(
+      <CheckboxQuestion
+        className="customClass"
+        icon={<Icon type="test" />}
+        onChange={onChange}
+        value={value}
+      />
+    )
+    expect(wrapper.find(Icon)).toHaveLength(1)
+    const cb = wrapper.find(Checkbox)
+    expect(cb.prop('className')).toMatch('checkbox')
+    expect(cb.prop('className')).toMatch('customClass')
+    expect(cb.prop('checked')).toBe(checked)
+    expect(cb.prop('indeterminate')).toBe(indeterminate)
+    cb.prop('onChange')({ target: { checked: false } })
+    cb.prop('onChange')({ target: { checked: true } })
+    expect(onChange).toBeCalledTimes(2)
+    expect(onChange).nthCalledWith(1, '0')
+    expect(onChange).nthCalledWith(2, '1')
+  })
+})
