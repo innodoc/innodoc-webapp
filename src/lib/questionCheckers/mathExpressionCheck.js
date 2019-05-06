96299b3f0193fc39b68f1384917d47cabe0aef3c
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-05-06 13:59:18 +0200

    replace mathjs with nerdamer

--- src/lib/questionCheckers/mathExpressionCheck.js
+++ src/lib/questionCheckers/mathExpressionCheck.js
@@ -1,23 +1,28 @@
-import math from 'mathjs'
+import nerdamer from 'nerdamer'
 
-const DEFAULT_PRECISION = 4
+// const DEFAULT_PRECISION = 4
 
-// TODO: is this mathematically correct (or exact same as mintmod?)
-const check = (input, solution, { precision }) => {
-  const epsilon = math.eval(`1e-${precision || DEFAULT_PRECISION}`)
-  math.config({ epsilon })
+const check = (input, solution) => {
+  // const precision = attributes.precision ||
   try {
-    const evalInput = math.eval(input)
-    const evalSolution = math.eval(solution)
-    if (evalInput) {
-      return math.equal(evalInput, evalSolution)
-    }
-  } catch (e) {
-    if (e instanceof SyntaxError) {
-      return false
-    }
+    return nerdamer(input).eq(solution)
+  } catch {
+    return false
   }
-  return false
+  // const epsilon = math.eval(`1e-${precision || DEFAULT_PRECISION}`)
+  // math.config({ epsilon })
+  // try {
+  //   const evalInput = math.eval(input)
+  //   const evalSolution = math.eval(solution)
+  //   if (evalInput) {
+  //     return math.equal(evalInput, evalSolution)
+  //   }
+  // } catch (e) {
+  //   if (e instanceof SyntaxError) {
+  //     return false
+  //   }
+  // }
+  // return false
 }
 
 export default check
