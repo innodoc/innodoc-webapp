74833b9ae6931613383ade9ae1cb1ff08c2e8406
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-05-06 13:59:18 +0200

    replace mathjs with nerdamer

--- src/lib/questionCheckers/mathFunctionCheck.js
+++ src/lib/questionCheckers/mathFunctionCheck.js
@@ -1,18 +1,22 @@
-import math from 'mathjs'
+import nerdamer from 'nerdamer'
 
 const check = (input, solution) => {
   try {
-    const parsedInput = math.simplify(math.parse(input))
-    const parsedSolution = math.simplify(math.parse(solution))
-    if (parsedInput) {
-      return parsedInput.equals(parsedSolution)
-    }
-  } catch (e) {
-    if (e instanceof SyntaxError) {
-      return false
-    }
+    return nerdamer(input).eq(solution)
+  } catch {
+    return false
   }
-  return false
+  // try {
+  //   const parsedInput = math.simplify(math.parse(input))
+  //   const parsedSolution = math.simplify(math.parse(solution))
+  //   if (parsedInput) {
+  //     return parsedInput.equals(parsedSolution)
+  //   }
+  // } catch (e) {
+  //   if (e instanceof SyntaxError) {
+  //     return false
+  //   }
+  // }
 }
 
 export default check
