6e5c67cc1aa698e171cfc5cff342ef2191d42338
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-07-04 17:08:19 +0200

    restore error page translation and convert to functional component

--- src/components/pages/error.js
+++ src/components/pages/error.js
@@ -3,42 +3,32 @@ import PropTypes from 'prop-types'
 import { Alert } from 'antd'
 
 import Layout from '../Layout'
+import { useTranslation } from '../../lib/i18n'
 
-class ErrorPage extends React.Component {
-  static getInitialProps({ res, err }) {
-    if (res) {
-      return { statusCode: res.statusCode }
-    }
-    if (err) {
-      return { statusCode: err.statusCode }
-    }
-    return {}
+const ErrorPage = ({ statusCode }) => {
+  const { t } = useTranslation()
+  return (
+    <Layout>
+      <Alert
+        message={t([`errorPage.${statusCode}.title`, 'errorPage.unspecific.title'])}
+        description={t([`errorPage.${statusCode}.msg`, 'errorPage.unspecific.msg'])}
+        type={statusCode === 404 ? 'info' : 'error'}
+        showIcon
+      />
+    </Layout>
+  )
+}
+
+ErrorPage.getInitialProps = ({ res, err }) => {
+  if (res) {
+    return { statusCode: res.statusCode }
   }
-
-  render() {
-    const { statusCode } = this.props
-    const descr = statusCode === 404
-      ? 'This page could not be found.'
-      : 'An error occured.'
-    return (
-      <Layout>
-        <Alert
-          message="Error"
-          description={descr}
-          type={statusCode === 404 ? 'info' : 'error'}
-          showIcon
-        />
-      </Layout>
-    )
+  if (err) {
+    return { statusCode: err.statusCode }
   }
+  return {}
 }
-
-ErrorPage.defaultProps = {
-  statusCode: null,
-}
-
-ErrorPage.propTypes = {
-  statusCode: PropTypes.number,
-}
+ErrorPage.defaultProps = { statusCode: null }
+ErrorPage.propTypes = { statusCode: PropTypes.number }
 
 export default ErrorPage
