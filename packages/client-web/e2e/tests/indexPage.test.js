04adf6f46ae085d7b959379ea99d7e48ee2625ff
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-09-09 14:06:22 +0200

    add E2E test for IndexPage

---
+++ packages/client-web/e2e/tests/indexPage.test.js
@@ -0,0 +1,10 @@
+describe('IndexPage', () => {
+  it('shows an index and index terms', async () => {
+    expect.assertions(4)
+    await page.goto(getUrl('/index-page'))
+    await expect(page).toMatchElement('h1', { text: 'Index' })
+    const row = await expect(page).toMatchElement('li', { text: 'Markdown' })
+    await expect(row).toMatchElement('a', { text: 'Project structure' })
+    await expect(row).toMatchElement('a', { text: 'Content files' })
+  })
+})
