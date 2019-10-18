8856d63d4cfad04f6dfd55db7d2cb5da586df3ab
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-10-18 14:53:48 +0200

    fix E2E tests

--- packages/client-web/e2e/setup.js
+++ packages/client-web/e2e/setup.js
@@ -1,7 +1,7 @@
 import { setDefaultOptions } from 'expect-puppeteer'
 import registerScreenshotReporter from './screenshotReporter'
 
-setDefaultOptions({ timeout: 2000 }) // for a single puppeteer command
+setDefaultOptions({ timeout: 5000 }) // for a single puppeteer command
 jest.setTimeout(15000) // for a single test
 
 // take screenshots on failed tests for better debugging
