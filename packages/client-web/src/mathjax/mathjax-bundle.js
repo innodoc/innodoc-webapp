ce99808bb628caa5103569c7def49ec3bb38d4f4
Mirko Dietrich <dietrich@math.tu-berlin.de>
2019-11-07 16:20:55 +0100

    tmp

--- packages/client-web/src/mathjax/mathjax-bundle.js
+++ packages/client-web/src/mathjax/mathjax-bundle.js
@@ -1,29 +1,12 @@
 require('mathjax-full/components/src/startup/lib/startup.js')
-const { Loader } = require('mathjax-full/js/components/loader')
-
-Loader.preLoad(
-  'loader',
-  'startup',
-  'core',
-
-  'input/tex-base',
-  '[tex]/ams',
-  '[tex]/newcommand',
-
-  'output/chtml',
-  'output/chtml/fonts/tex.js'
-)
-
 require('mathjax-full/components/src/core/core.js')
 
 require('mathjax-full/components/src/input/tex-base/tex-base.js')
 require('mathjax-full/components/src/input/tex/extensions/ams/ams.js')
 require('mathjax-full/components/src/input/tex/extensions/newcommand/newcommand.js')
-
-window.MathJax.config.tex.packages.push('ams', 'newcommand')
+// require('./innodoc.js')
 
 require('mathjax-full/components/src/output/chtml/chtml.js')
 require('mathjax-full/components/src/output/chtml/fonts/tex/tex.js')
 
-// This will cause all the normal startup operations to be performed.
 require('mathjax-full/components/src/startup/startup.js')
