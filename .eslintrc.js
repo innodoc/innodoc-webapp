251ef02f9cf70d9748a9484661c1289e765ae485
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-01-09 17:14:46 +0100

    Add prettier (closes #99)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,35 +5,28 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  extends: [
-    'airbnb',
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 7,
     sourceType: 'module',
   },
-  plugins: ['jest'],
+  plugins: ['jest', 'prettier'],
   root: true,
   rules: {
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'ignore',
-    }],
-    'function-paren-newline': ['off'],
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/no-disabled-tests': 'off',
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
-    'jsx-a11y/anchor-is-valid': ['off'],
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.js'],
-    }],
-    'react/forbid-prop-types': ['off'],
-    semi: ['error', 'never'],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'react/forbid-prop-types': 'off',
+    'prettier/prettier': 'error',
   },
 }
