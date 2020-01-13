2cb23d9d3ad4533782d79d41ca1f96a5dfd25ce2
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-01-13 11:50:02 +0100

    Fix test: client-web/src/components/content/ContentFragment/astComponents/Math.test.js

--- packages/client-web/src/components/content/ContentFragment/astComponents/Math.test.js
+++ packages/client-web/src/components/content/ContentFragment/astComponents/Math.test.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import MathJax from '@innodoc/react-mathjax-node'
 
 import Math from './Math'
-import { mathDelimiter } from '../../../../hooks/useMathJax'
-
-jest.mock('../../../../hooks/useMathJax', () => ({
-  mathDelimiter: jest.requireActual('../../../../hooks/useMathJax')
-    .mathDelimiter,
-}))
 
 const getData = (mathType) => [{ t: mathType }, 'x^2']
 
 describe('<Math />', () => {
-  it.each([
-    ['InlineMath', 'inline'],
-    ['DisplayMath', 'display'],
-  ])('should render (%s)', (mathType, cls) => {
+  it.each(['InlineMath', 'DisplayMath'])('should render (%s)', (mathType) => {
     const wrapper = shallow(<Math data={getData(mathType)} />)
-    const span = wrapper.find('span')
-    expect(span).toHaveLength(1)
-    expect(span.hasClass('math')).toBe(true)
-    expect(span.hasClass(cls)).toBe(true)
-    const delims = mathDelimiter[cls]
-    expect(span.text()).toBe(`${delims[0]}x^2${delims[1]}`)
+    const ExpectedComponent =
+      mathType === 'InlineMath' ? MathJax.Span : MathJax.Div
+    const component = wrapper.find(ExpectedComponent)
+    expect(component).toHaveLength(1)
+    expect(component.prop('texCode')).toBe('x^2')
   })
 })
