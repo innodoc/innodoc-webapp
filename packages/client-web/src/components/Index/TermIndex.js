2110e7ec4407c7c1a57d61827ca54d3e8e79ce78
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-01-15 16:23:56 +0100

    Refactor and fix TermIndex (#15)

--- packages/client-web/src/components/Index/TermIndex.js
+++ packages/client-web/src/components/Index/TermIndex.js
@@ -11,34 +11,10 @@ import fadeInCss from '@innodoc/client-web/src/style/fade-in.sss'
 import { SectionLink } from '../content/links'
 import css from './style.sss'
 
-const parseName = (term) => {
-  const name = []
-  let index = 0
-  let remain = term
-  while (remain.length > 0) {
-    let match = remain.match(/^([^$]+)/)
-    if (match) {
-      const [, content] = match
-      name.push(<span key={index}>{content}</span>)
-      remain = remain.slice(content.length)
-      index += 1
-    } else {
-      match = remain.match(/^\$([^$]+)\$/)
-      if (match) {
-        const [, content] = match
-        name.push(<MathJax.Span key={index} texCode={content} />)
-        remain = remain.slice(content.length + 2)
-        index += 1
-      } else {
-        throw new Error(`Could not parse index term: ${term}`)
-      }
-    }
-  }
-  return name
-}
+import parseTermName from './parseTermName'
 
 const renderTerm = (term) => {
-  const title = <strong>{parseName(term.name)}</strong>
+  const title = <strong>{parseTermName(term.name)}</strong>
   const links = term.locations.map((id) => (
     <Tag key={id}>
       <SectionLink contentId={id} />
