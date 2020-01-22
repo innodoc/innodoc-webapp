e1ce5b2b7bbeaf9a02a50359808d4ad709550b1c
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-01-22 17:40:24 +0100

    Add next-sugarss

--- packages/client-web/nextConfig/index.js
+++ packages/client-web/nextConfig/index.js
@@ -1,11 +1,12 @@
 const withLess = require('@zeit/next-less')
+const withSugarSS = require('next-sugarss')
 const withTranspileModules = require('next-transpile-modules')
 
 const withBundleAnalyzer = require('./withBundleAnalyzer')
 const options = require('./options')
 
-// Next.js configuration
-const config = withLess(withTranspileModules(options))
+// Order of wrappers affects the order of the stylesheets
+const config = withSugarSS(withLess(withTranspileModules(options)))
 
 module.exports = process.env.BUNDLE_ANALYZE
   ? withBundleAnalyzer(config)
