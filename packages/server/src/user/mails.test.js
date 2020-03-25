77208cf4b49446476722dd538cb266735c465229
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-03-25 15:48:41 +0100

    Add server tests: verifyAccessTokenMiddleware.js, mails.js

--- packages/server/src/user/mails.test.js
+++ packages/server/src/user/mails.test.js
@@ -1,7 +1,39 @@
+import { verificationMail, resetPasswordMail } from './mails'
+
+const t = (s, int) => {
+  if (int) {
+    const intStr = Object.keys(int).reduce(
+      (acc, key) => `${acc}${key}=${int[key]}`,
+      ''
+    )
+    return `${s}_${intStr}`
+  }
+  return s
+}
+const appRoot = 'https://app.example.com/'
+
 describe('verificationMail', () => {
-  it.todo('should generate mail')
+  it('should generate mail', () => {
+    expect(
+      verificationMail(t, appRoot, 'alice@example.com', '123abcdef')
+    ).toEqual({
+      to: 'alice@example.com',
+      subject: 'emails.emailVerification.subject',
+      text:
+        'emails.emailVerification.text_verificationLink=https://app.example.com/verify-user/123abcdef',
+    })
+  })
 })
 
 describe('resetPasswordMail', () => {
-  it.todo('should generate mail')
+  it('should generate mail', () => {
+    expect(
+      resetPasswordMail(t, appRoot, 'alice@example.com', '123abcdef')
+    ).toEqual({
+      to: 'alice@example.com',
+      subject: 'emails.passwordReset.subject',
+      text:
+        'emails.passwordReset.text_passwordResetLink=https://app.example.com/reset-password/123abcdef',
+    })
+  })
 })
