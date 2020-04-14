9211cb6da1f48e697517ef4f932552623f7ee6c2
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-04-14 12:17:57 +0200

    tmp fix ci

--- packages/client-web/e2e/jest/jest.config.js
+++ packages/client-web/e2e/jest/jest.config.js
@@ -4,6 +4,8 @@ const rootDir = resolve(__dirname, '..', '..')
 const e2eJestDir = resolve(rootDir, 'e2e', 'jest')
 
 module.exports = {
+  globalSetup: resolve(e2eJestDir, 'globalSetup.js'),
+  globalTeardown: resolve(e2eJestDir, 'globalTeardown.js'),
   rootDir,
   roots: [resolve(rootDir, 'e2e', 'tests')],
   setupFilesAfterEnv: [resolve(e2eJestDir, 'setupAfterEnv.js')],
