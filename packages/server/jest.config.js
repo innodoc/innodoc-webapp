8599ecfc9bc4eb33bb58e7f1f06b8098bdeccdf2
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-04-14 13:04:21 +0200

    Use @shelf/jest-mongodb only for non-CI (server)

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  preset: '@shelf/jest-mongodb',
+const fs = require('fs')
+
+const config = {
   roots: ['src'],
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
   verbose: true,
 }
+
+if (fs.existsSync('/etc/alpine-release')) {
+  config.testEnvironment = 'node'
+} else {
+  config.preset = '@shelf/jest-mongodb'
+}
+
+module.exports = config
