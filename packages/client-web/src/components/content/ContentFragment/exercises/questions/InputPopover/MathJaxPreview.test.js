5c906c71b9f8092d3662ac726803f38f2679ec43
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-06-10 10:22:41 +0200

    Add tests for <MathJaxPreview />

--- packages/client-web/src/components/content/ContentFragment/exercises/questions/InputPopover/MathJaxPreview.test.js
+++ packages/client-web/src/components/content/ContentFragment/exercises/questions/InputPopover/MathJaxPreview.test.js
@@ -1,1 +1,20 @@
-// TODO
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import MathJax from '@innodoc/react-mathjax-node'
+
+import MathJaxPreview from './MathJaxPreview'
+
+describe('<MathJaxPreview />', () => {
+  it('should render', () => {
+    const wrapper = shallow(<MathJaxPreview texCode="x^2" />)
+    const node = wrapper.find(MathJax.MathJaxNode)
+    expect(node.prop('displayType')).toBe('display')
+    expect(node.prop('texCode')).toBe('x^2')
+  })
+
+  it('should render empty with empty texCode', () => {
+    const wrapper = shallow(<MathJaxPreview texCode="" />)
+    expect(wrapper.isEmptyRender()).toBe(true)
+  })
+})
