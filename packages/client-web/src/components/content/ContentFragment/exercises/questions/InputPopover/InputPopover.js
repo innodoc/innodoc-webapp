789ea0838c2e56ba28bb0b8163b7b88c94886da5
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-06-11 14:54:44 +0200

    Add label to LaTeX preview in popover, add popover max width

--- packages/client-web/src/components/content/ContentFragment/exercises/questions/InputPopover/InputPopover.js
+++ packages/client-web/src/components/content/ContentFragment/exercises/questions/InputPopover/InputPopover.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Grid, Popover } from 'antd'
 
+import { useTranslation } from '@innodoc/client-misc/src/i18n'
 import {
   childrenType,
   feedbackMessagesType,
@@ -18,11 +19,13 @@ const InputPopover = ({
   showPreview,
   userInput,
 }) => {
+  const { t } = useTranslation()
   const { lg } = Grid.useBreakpoint()
   const hasData = Boolean(messages.length) || Boolean(userInput.length)
 
   const preview = showPreview ? (
     <div className={css.mathJaxPreview}>
+      <div className={css.previewLabel}>{t('questions.preview')}</div>
       <MathJaxPreview texCode={userInput || ''} />
     </div>
   ) : null
