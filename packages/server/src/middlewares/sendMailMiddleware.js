b64f7cd25a864c24df175d5092e19a77c42a0903
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-07-16 19:02:05 +0200

    Allow SMTP w/o auth

--- packages/server/src/middlewares/sendMailMiddleware.js
+++ packages/server/src/middlewares/sendMailMiddleware.js
@@ -12,11 +12,11 @@ const sendMailMiddleware = ({
     // Skip mail sending entirely (for E2E testing)
     req.app.locals.sendMail = () => {}
   } else {
-    const transport = nodemailer.createTransport({
-      host,
-      port,
-      auth: { user, pass: password },
-    })
+    const transportOpts = { host, port }
+    if (user && password) {
+      transportOpts.auth = { user, pass: password }
+    }
+    const transport = nodemailer.createTransport(transportOpts)
     req.app.locals.sendMail = (params) =>
       transport.sendMail({
         ...params,
