1e9f544a15de88831093a6b617dd9fe9b99e2ce0
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-08-20 18:23:54 +0200

    Fix packages/client-web/src/components/Layout/Layout.test.js

--- packages/client-web/src/components/Layout/Layout.js
+++ packages/client-web/src/components/Layout/Layout.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useDispatch, useSelector } from 'react-redux'
 import { Layout as AntLayout } from 'antd'
-import cookie from 'react-cookies'
+import cookies from 'react-cookies'
 
 import { childrenType } from '@innodoc/client-misc/src/propTypes'
 import { closeMessage } from '@innodoc/client-store/src/actions/ui'
 import appSelectors from '@innodoc/client-store/src/selectors'
 import userMessageSelectors from '@innodoc/client-store/src/selectors/userMessage'
 
-import Footer from './Footer'
 import DataProtectionModal from './DataProtectionModal'
+import Footer from './Footer'
 import Header from './Header'
 import MessageModal from './MessageModal'
 import Sidebar from './Sidebar'
@@ -26,16 +26,19 @@ const Layout = ({ children, disableSidebar }) => {
   const [showConsentModal, setShowConsent] = useState(false)
   const [consentGiven, setConsentGiven] = useState(false)
 
+  // Show consent dialog on first view.
   useEffect(() => {
-    if (!loggedInEmail && !cookie.load(DATA_CONSENT_ACCESS_COOKIE)) {
+    if (!loggedInEmail && !cookies.load(DATA_CONSENT_ACCESS_COOKIE)) {
       setShowConsent(true)
     }
   }, [loggedInEmail])
+
+  // Save consent to cookie on accept.
   useEffect(() => {
     if (consentGiven) {
       const now = new Date()
       const expires = new Date(now.getFullYear() + 100, now.getMonth(), now.getDate())
-      cookie.save(DATA_CONSENT_ACCESS_COOKIE, true, {
+      cookies.save(DATA_CONSENT_ACCESS_COOKIE, true, {
         expires,
         maxAge: Math.round(expires - now / 1000),
         path: '/',
@@ -52,7 +55,7 @@ const Layout = ({ children, disableSidebar }) => {
   )
 
   const modal = message ? (
-    <MessageModal message={message} onAccept={() => dispatch(closeMessage(message.id))} />
+    <MessageModal message={message} onClose={() => dispatch(closeMessage(message.id))} />
   ) : null
 
   const dataProtectionModal = showConsentModal ? (
--- packages/client-web/src/components/Layout/Layout.test.js
+++ packages/client-web/src/components/Layout/Layout.test.js
@@ -1,24 +1,42 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
+import cookies from 'react-cookies'
 
-import Layout from './Layout'
-import Sidebar from './Sidebar'
-import Header from './Header'
+import appSelectors from '@innodoc/client-store/src/selectors'
+
+import DataProtectionModal from './DataProtectionModal'
 import Footer from './Footer'
+import Header from './Header'
+import Layout from './Layout'
 import MessageModal from './MessageModal'
+import Sidebar from './Sidebar'
 
-// TODO update
-
+let mockLoggedInEmail
 let mockLatestMessage
 const mockDispatch = jest.fn()
+const mockAppSelectors = appSelectors
 jest.mock('react-redux', () => ({
   useDispatch: () => mockDispatch,
-  useSelector: () => mockLatestMessage,
+  useSelector: (selector) =>
+    selector === mockAppSelectors.getApp ? { loggedInEmail: mockLoggedInEmail } : mockLatestMessage,
 }))
+
+let mockCookieVal
+jest.mock('react-cookies', () => ({
+  load: () => mockCookieVal,
+  save: jest.fn(),
+}))
+
+jest.mock('./DataProtectionModal', () => () => null)
+jest.mock('./Footer', () => () => null)
+jest.mock('../Toc', () => () => null)
+jest.mock('./Sidebar', () => () => null)
+jest.mock('./Header', () => () => null)
 
 describe('<Layout />', () => {
   beforeEach(() => {
     mockLatestMessage = undefined
+    mockLoggedInEmail = 'alice@example.com'
   })
 
   describe('default', () => {
@@ -46,7 +64,55 @@ describe('<Layout />', () => {
     })
   })
 
-  describe('with message', () => {
+  describe('data protection modal', () => {
+    beforeEach(() => {
+      mockLoggedInEmail = undefined
+      mockCookieVal = undefined
+    })
+
+    it('should show modal on first view', () => {
+      const wrapper = mount(
+        <Layout>
+          <div />
+        </Layout>
+      )
+      expect(wrapper.exists(DataProtectionModal)).toBe(true)
+    })
+
+    it('should not show modal if logged in', () => {
+      mockLoggedInEmail = 'alice@example.com'
+      const wrapper = mount(
+        <Layout>
+          <div />
+        </Layout>
+      )
+      expect(wrapper.exists(DataProtectionModal)).toBe(false)
+    })
+
+    it('should not show modal if consent cookie is present', () => {
+      mockCookieVal = true
+      const wrapper = mount(
+        <Layout>
+          <div />
+        </Layout>
+      )
+      expect(wrapper.exists(DataProtectionModal)).toBe(false)
+    })
+
+    it('should save cookie and close modal if consent is given', () => {
+      const wrapper = mount(
+        <Layout>
+          <div />
+        </Layout>
+      )
+      const modal = wrapper.find(DataProtectionModal)
+      modal.invoke('onAccept')()
+      expect(cookies.save).toBeCalledTimes(1)
+      expect(wrapper.exists(DataProtectionModal)).toBe(false)
+    })
+  })
+
+  describe('message modal', () => {
     beforeEach(() => {
       mockDispatch.mockClear()
       mockLatestMessage = {
