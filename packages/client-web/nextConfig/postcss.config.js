20f4bce9fb14291fb6b7c3ec34e4d4805de937c5
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-09-01 19:59:55 +0200

    Use require.resolve in postcss.config.js

--- packages/client-web/nextConfig/postcss.config.js
+++ packages/client-web/nextConfig/postcss.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 
 module.exports = ({ disablePostcssImportJson, file: { extname } }) => ({
-  parser: extname === '.sss' ? 'sugarss' : undefined,
+  parser: extname === '.sss' ? require.resolve('sugarss') : undefined,
   plugins: {
-    'postcss-import': {},
+    [require.resolve('postcss-import')]: {},
     [path.resolve(__dirname, 'postcss-import-json')]: disablePostcssImportJson ? false : {},
-    'postcss-mixins': {},
-    precss: {},
-    'postcss-color-function': {},
+    [require.resolve('postcss-mixins')]: {},
+    [require.resolve('precss')]: {},
+    [require.resolve('postcss-color-function')]: {},
   },
 })
