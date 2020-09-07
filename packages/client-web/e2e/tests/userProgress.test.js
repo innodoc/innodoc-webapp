a731a43a2e9d8abad043512cb9153c5d83278583
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-09-07 17:55:47 +0200

    tmp

--- packages/client-web/e2e/tests/userProgress.test.js
+++ packages/client-web/e2e/tests/userProgress.test.js
@@ -19,43 +19,47 @@ describe('User progress', () => {
     await browser.wait(2500)
   }
 
-  test('Local storage', async () => {
-    await openUrl('progress')
-    await assertProgress(0, 0, ['0 %', 'Visited 0 out of 7.'])
-    await assertProgress(1, 0, ['0 %', 'Visited 0 out of 17.'])
-    await assertProgress(1, 1, ['0 %', 'Scored 0 out of 63 points.'])
-    await solveExercise(0, 'solution')
-    await openUrl('progress')
-    await assertProgress(0, 0, ['0 %', 'Visited 0 out of 7.'])
-    await assertProgress(1, 0, ['6 %', 'Visited 1 out of 17.'])
-    await assertProgress(1, 1, ['5 %', 'Scored 3 out of 63 points.'])
-    await openUrl('section/02-elements/02-headings')
-    await browser.wait(2500)
-    await openUrl('progress')
-    await assertProgress(0, 0, ['0 %', 'Visited 0 out of 7.'])
-    await assertProgress(1, 0, ['12 %', 'Visited 2 out of 17.'])
-    await assertProgress(1, 1, ['5 %', 'Scored 3 out of 63 points.'])
-  })
+  // test('Local storage', async () => {
+  //   await openUrl('progress')
+  //   await assertProgress(0, 0, ['0 %', 'Visited 0 out of 7.'])
+  //   await assertProgress(1, 0, ['0 %', 'Visited 0 out of 17.'])
+  //   await assertProgress(1, 1, ['0 %', 'Scored 0 out of 63 points.'])
+  //   await solveExercise(0, 'solution')
+  //   await openUrl('progress')
+  //   await assertProgress(0, 0, ['0 %', 'Visited 0 out of 7.'])
+  //   await assertProgress(1, 0, ['6 %', 'Visited 1 out of 17.'])
+  //   await assertProgress(1, 1, ['5 %', 'Scored 3 out of 63 points.'])
+  //   await openUrl('section/02-elements/02-headings')
+  //   await browser.wait(2500)
+  //   await openUrl('progress')
+  //   await assertProgress(0, 0, ['0 %', 'Visited 0 out of 7.'])
+  //   await assertProgress(1, 0, ['12 %', 'Visited 2 out of 17.'])
+  //   await assertProgress(1, 1, ['5 %', 'Scored 3 out of 63 points.'])
+  // })
 
   test('Server storage', async () => {
     await openUrl('progress')
     await assertProgress(1, 0, ['0 %', 'Visited 0 out of 17.'])
     await assertProgress(1, 1, ['0 %', 'Scored 0 out of 63 points.'])
+    console.log('solving first')
     await solveExercise(0, 'solution') // 3 points
     await openUrl('progress')
     await assertProgress(1, 0, ['6 %', 'Visited 1 out of 17.'])
     await assertProgress(1, 1, ['5 %', 'Scored 3 out of 63 points.'])
 
+    console.log('registering')
     const email = getRandEmail()
     await register(email, pwd)
     await activate(email)
     await login(email, pwd)
 
+    console.log('visiting another')
     await openUrl('section/02-elements/02-headings')
     await browser.wait(2500)
     await openUrl('progress')
     await assertProgress(1, 0, ['12 %', 'Visited 2 out of 17.'])
     await assertProgress(1, 1, ['5 %', 'Scored 3 out of 63 points.'])
+    console.log('solve another')
     await solveExercise(1, '5') // 4 points
     await openUrl('progress')
     await assertProgress(1, 0, ['12 %', 'Visited 2 out of 17.'])
@@ -63,12 +67,15 @@ describe('User progress', () => {
     await logout(email)
 
     // Empty progress after logout
+    console.log('logged out')
     await openUrl('progress')
     await assertProgress(1, 0, ['0 %', 'Visited 0 out of 17.'])
     await assertProgress(1, 1, ['0 %', 'Scored 0 out of 63 points.'])
 
+    console.log('visiting one')
     await openUrl('section/02-elements/01-formatting')
     await browser.wait(2500)
+    console.log('solve two exercises, one incorrect')
     await solveExercise(0, 'wrong')
     await solveExercise(2, '{1}') // 4 points
     await openUrl('progress')
@@ -76,6 +83,7 @@ describe('User progress', () => {
     await assertProgress(1, 1, ['6 %', 'Scored 4 out of 63 points.'])
 
     // Check progress merge on login
+    console.log('login again')
     await login(email, pwd)
     await openUrl('progress')
     await assertProgress(1, 0, ['18 %', 'Visited 3 out of 17.'])
