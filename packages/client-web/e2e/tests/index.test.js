a280735db4737ab1ac878955d6af1b214ba472e0
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-09-15 13:09:16 +0200

    E2E: Fix redirect status code in index.test.js

--- packages/client-web/e2e/tests/index.test.js
+++ packages/client-web/e2e/tests/index.test.js
@@ -7,7 +7,7 @@ describe('Index', () => {
     const chain = req.redirectChain()
     expect(chain).toHaveLength(1)
     const redirectResponse = await chain[0].response()
-    expect(redirectResponse.status()).toBe(301)
+    expect(redirectResponse.status()).toBe(308)
     expect(redirectResponse.headers().location).toBe('/page/about')
     expect(await browser.text('h1')).toContain('About this course')
     expect(await browser.title()).toBe('About this course · innoDoc')
