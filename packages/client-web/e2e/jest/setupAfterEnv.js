42993e40fcc4bae47e68f64946c5fe00fc35503d
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-10-13 18:13:22 +0200

    E2E: Increase jest timeout to 120 seconds

--- packages/client-web/e2e/jest/setupAfterEnv.js
+++ packages/client-web/e2e/jest/setupAfterEnv.js
@@ -1,7 +1,7 @@
 const headless = process.env.PLAYWRIGHT_HEADLESS !== 'false'
 const isCI = process.env.INNODOC_WEBAPP_CI === 'true'
 
-let jestTimeout = 1000 * 60
+let jestTimeout = 1000 * 120
 
 if (!headless || isCI) {
   jestTimeout *= 5
