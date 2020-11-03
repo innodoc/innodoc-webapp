0f4e8aaa4443294054f93cba64be1afaa489a2de
Mirko Dietrich <dietrich@math.tu-berlin.de>
2020-11-03 12:25:19 +0100

    Set min-width CSS property on plain AST images

--- packages/client-web/src/components/content/ContentFragment/astComponents/Image.js
+++ packages/client-web/src/components/content/ContentFragment/astComponents/Image.js
@@ -5,6 +5,8 @@ import { useSelector } from 'react-redux'
 import appSelectors from '@innodoc/client-store/src/selectors'
 import { astToString } from '@innodoc/client-misc/src/util'
 
+import css from './style.sss'
+
 const Image = ({ data }) => {
   const { staticRoot } = useSelector(appSelectors.getApp)
 
@@ -12,7 +14,7 @@ const Image = ({ data }) => {
     const [[id], content, [src, alt]] = data
     const imgSrc = /^https?:\/\//i.test(src) ? src : `${staticRoot}${src}`
     const imgAlt = alt || astToString(content)
-    return <img id={id} src={imgSrc} alt={imgAlt} />
+    return <img className={css.image} id={id} src={imgSrc} alt={imgAlt} />
   } catch (err) {
     return null
   }
