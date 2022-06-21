1af1ba4e47a19d92b4c5fd7c752fed0fa8bff69b
Mirko Dietrich <dietrich@math.tu-berlin.de>
2022-06-21 16:47:03 +0200

    Migrate postcss-import-json plugin to PostCSS 8

--- packages/client-web/nextConfig/postcss-import-json.js
+++ packages/client-web/nextConfig/postcss-import-json.js
@@ -1,40 +1,38 @@
-const { readFile } = require('fs')
+const { readFileSync } = require('fs')
 const path = require('path')
-const postcss = require('postcss')
 
-// Implement @import-json rule that imports variables from JSON file.
-module.exports = postcss.plugin('postcss-import-json', () => (root) => {
-  const rulePromises = []
-  root.walkAtRules('import-json', (rule) => {
-    const importParam = rule.params.replace(/^['"](.+)['"]$/, '$1')
-    const importFilepath = path.resolve(path.dirname(rule.source.input.file), importParam)
-    rulePromises.push(
-      new Promise((resolve, reject) =>
-        readFile(importFilepath, (err, fileData) => {
-          if (err) {
-            reject(err)
-          } else {
-            try {
-              const vars = JSON.parse(fileData)
-              const varNames = Object.keys(vars)
-              for (let i = 0; i < varNames.length; i += 1) {
-                const varName = varNames[i]
-                const varNode = postcss.decl({
-                  prop: `$${varName}`,
-                  value: vars[varName],
-                })
-                varNode.source = rule.source
-                rule.parent.insertBefore(rule, varNode)
-              }
-              rule.remove()
-              resolve()
-            } catch (e) {
-              reject(e)
-            }
-          }
-        })
+// @import-json rule that imports variables from JSON file.
+module.exports = () => ({
+  postcssPlugin: 'postcss-import-json',
+  Once: (root, { decl, result }) => {
+    root.walkAtRules('import-json', (atRule) => {
+      const importParam = atRule.params.replace(/^['"](.+)['"]$/, '$1')
+      const importFilepath = path.resolve(path.dirname(atRule.source.input.file), importParam)
+
+      const fileData = readFileSync(importFilepath)
+
+      result.messages.push({
+        type: 'dependency',
+        plugin: 'postcss-import-json',
+        file: importFilepath,
+        parent: result.opts.from,
+      })
+
+      const varList = JSON.parse(fileData)
+      atRule.parent.insertBefore(
+        atRule,
+        Object.keys(varList).map((varName) =>
+          decl({
+            prop: `$${varName}`,
+            value: varList[varName],
+            source: atRule.source,
+          })
+        )
       )
-    )
-  })
-  return Promise.all(rulePromises)
+
+      atRule.remove()
+    })
+  },
 })
+
+module.exports.postcss = true
