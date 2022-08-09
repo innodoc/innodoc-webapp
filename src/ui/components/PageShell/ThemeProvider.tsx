723e4b6a2a4b1cd5dfbf6e86784face90c88cf78
Mirko Dietrich <dietrich@math.tu-berlin.de>
2022-08-09 10:48:25 +0200

    Scale down header font sizes further

--- src/ui/components/PageShell/ThemeProvider.tsx
+++ src/ui/components/PageShell/ThemeProvider.tsx
@@ -15,13 +15,13 @@ function makeTheme(paletteMode: PaletteMode) {
     createTheme({
       palette: { mode: paletteMode },
       typography: {
-        // Scale down huge MUI font sizes using new=(orig-1)*0.4+1
-        h1: { fontSize: '3rem' },
-        h2: { fontSize: '2.1rem' },
-        h3: { fontSize: '1.8rem' },
-        h4: { fontSize: '1.45rem' },
-        h5: { fontSize: '1.2rem' },
-        h6: { fontSize: '1.1rem' },
+        // Scale down huge MUI font sizes
+        h1: { fontSize: '2rem' },
+        h2: { fontSize: '1.75rem' },
+        h3: { fontSize: '1.5rem' },
+        h4: { fontSize: '1.3rem' },
+        h5: { fontSize: '1.1rem' },
+        h6: { fontSize: '1.05rem' },
       },
     })
   )
