68d02725cd9ff9fe2cc8cab223e80d5e0b8d0184
Mirko Dietrich <dietrich@math.tu-berlin.de>
2022-11-02 19:04:53 +0100

    Handle style attribute in <SvgElementNode>

--- src/ui/components/common/SvgNode.tsx
+++ src/ui/components/common/SvgNode.tsx
@@ -1,18 +1,33 @@
-import { createElement } from 'react'
+import { createElement, useEffect, useRef } from 'react'
 import type { ElementNode } from 'svg-parser'
 
 export function isElementNode(node: unknown): node is ElementNode {
   return node !== null && typeof node === 'object' && (node as ElementNode).type === 'element'
 }
 
-function SvgElementNode({ node: { tagName, properties = {}, children } }: SvgElementNodeProps) {
-  return tagName !== undefined
-    ? createElement(
-        tagName,
-        properties,
-        children.filter(isElementNode).map((node, idx) => <SvgElementNode key={idx} node={node} />)
-      )
-    : null
+function SvgElementNode({
+  node: { tagName, properties: { style, ...otherProperties } = {}, children },
+}: SvgElementNodeProps) {
+  // Need to set style attr as string val
+  const ref = useRef<SVGPathElement>()
+  useEffect(() => {
+    if (ref.current !== undefined && typeof style === 'string') {
+      ref.current.setAttribute('style', style)
+    }
+  }, [style])
+
+  const a =
+    tagName !== undefined
+      ? createElement(
+          tagName,
+          { ...otherProperties, ref },
+          children
+            .filter(isElementNode)
+            .map((node, idx) => <SvgElementNode key={idx} node={node} />)
+        )
+      : null
+
+  return a
 }
 
 type SvgElementNodeProps = {
