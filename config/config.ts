293402cf74a669e52b1885d6289dbc33293824b3
Mirko Dietrich <dietrich@math.tu-berlin.de>
2022-11-16 11:58:23 +0100

    Unit tests run without content webserver

--- config/config.ts
+++ config/config.ts
@@ -2,21 +2,16 @@ import path from 'path'
 
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
-import type { UserConfigExport } from 'vite'
 import ssr from 'vite-plugin-ssr/plugin'
-import type { InlineConfig as VitestInlineConfig } from 'vitest'
+import { type InlineConfig as VitestInlineConfig } from 'vitest'
+import { type UserConfigExport } from 'vitest/config'
 
 import buildData from './buildData'
 
 dotenv.config()
 
 /* Configure tests */
-function testConfig() {
-  const testMode = process.env.VITEST_MODE ?? 'unit'
-  if (!['integration', 'unit'].includes(testMode)) {
-    throw new Error('VITEST_MODE must be one of integration or unit.')
-  }
-
+function testConfig(testMode: string) {
   const config: VitestInlineConfig = {
     globals: true,
     include: [`tests/${testMode}/**/*.test.{ts,tsx}`],
@@ -31,10 +26,14 @@ function testConfig() {
 }
 
 /* vite configuration */
-async function config(): Promise<UserConfigExport> {
-  await buildData()
+async function config() {
+  const testMode = process.env.VITEST_MODE
 
-  return {
+  if (testMode !== 'unit') {
+    await buildData() // Don't require running web server for unit tests
+  }
+
+  const config: UserConfigExport = {
     envPrefix: 'INNODOC_', // Exposed to client
     plugins: [react(), ssr({ prerender: true })],
     resolve: {
@@ -48,8 +47,13 @@ async function config(): Promise<UserConfigExport> {
         '@reduxjs/toolkit', // otherwise can't be loaded on prerendering
       ],
     },
-    test: testConfig(),
   }
+
+  if (testMode && ['integration', 'unit'].includes(testMode)) {
+    config.test = testConfig(testMode)
+  }
+
+  return config
 }
 
 export default config
