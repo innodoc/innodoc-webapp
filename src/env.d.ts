7fa4f83ab224d8ff4f8dd2c614cce0438e39f0d1
Mirko Dietrich <dietrich@math.tu-berlin.de>
2023-02-16 15:29:29 +0100

    Tidy up env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,17 +4,11 @@ interface ImportMetaEnv {
   /** Application base URL */
   readonly INNODOC_APP_ROOT: string
 
-  /** Base URL for content (e.g. `https://example.com/content`) */
-  readonly INNODOC_CONTENT_ROOT: string
-
   /** URL path prefix for content pages (e.g. `page`) */
   readonly INNODOC_PAGE_PATH_PREFIX: string
 
   /** URL path prefix for sections (e.g. `section`) */
   readonly INNODOC_SECTION_PATH_PREFIX: string
-
-  /** URL to load static files from */
-  readonly INNODOC_STATIC_ROOT: string
 }
 
 interface ImportMeta {
