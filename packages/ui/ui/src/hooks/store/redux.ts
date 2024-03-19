cc83a7f70006aca1cf8756cb100e238a4bb15961
Mirko Dietrich <dietrich@math.tu-berlin.de>
2024-03-20 00:33:58 +0100

    refactor: use new `withTypes()` for creating typed `useDispatch`/`useSelector`

--- packages/ui/ui/src/hooks/store/redux.ts
+++ packages/ui/ui/src/hooks/store/redux.ts
@@ -1,13 +1,9 @@
-/* eslint-disable @typescript-eslint/no-restricted-imports */
-import {
-  type TypedUseSelectorHook,
-  useDispatch as useDispatchReactRedux,
-  useSelector as useSelectorReactRedux,
-} from 'react-redux'
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import { useDispatch as useDispatchReactRedux, useSelector as useSelectorReactRedux } from 'react-redux'
 
 import type { AppDispatch, RootState } from '@innodoc/store/types'
 
-const useDispatch: () => AppDispatch = useDispatchReactRedux
-const useSelector: TypedUseSelectorHook<RootState> = useSelectorReactRedux
+const useDispatch = useDispatchReactRedux.withTypes<AppDispatch>()
+const useSelector = useSelectorReactRedux.withTypes<RootState>()
 
 export { useDispatch, useSelector }
