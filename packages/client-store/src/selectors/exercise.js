f970adf48b87d26acdd32253572d7010dff1cbbf
Mirko Dietrich <dietrich@math.tu-berlin.de>
2025-02-08 17:52:35 +0100

    Some more undefined checks

--- packages/client-store/src/selectors/exercise.js
+++ packages/client-store/src/selectors/exercise.js
@@ -9,10 +9,10 @@ const getExercise = createSelector(orm, selectId, (session, id) => {
   const exercise = session.Exercise.withId(id)
   const questions = session.Question.filter({ exerciseId: id }).toRefArray()
   const answeredQuestions = questions.filter((q) => q.result !== RESULT_VALUE.NEUTRAL)
-  const isAnswered = answeredQuestions && answeredQuestions.length === exercise.questionCount
+  const isAnswered = answeredQuestions && answeredQuestions.length === (exercise ? exercise.questionCount : 0)
 
   return {
-    id: exercise.id,
+    id: exercise ? exercise.id : 0,
     isAnswered,
     isCorrect: isAnswered && answeredQuestions.every((q) => q.result === RESULT_VALUE.CORRECT),
     isTouched: questions.some((q) => q.result !== RESULT_VALUE.NEUTRAL),
