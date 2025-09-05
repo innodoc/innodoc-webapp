161aadd99ad927895c6c04bbc9f388d587a50440
Mirko Dietrich <dietrich@math.tu-berlin.de>
2025-09-05 12:32:25 +0200

    Fix formatting

--- packages/client-store/src/selectors/exercise.js
+++ packages/client-store/src/selectors/exercise.js
@@ -9,7 +9,8 @@ const getExercise = createSelector(orm, selectId, (session, id) => {
   const exercise = session.Exercise.withId(id)
   const questions = session.Question.filter({ exerciseId: id }).toRefArray()
   const answeredQuestions = questions.filter((q) => q.result !== RESULT_VALUE.NEUTRAL)
-  const isAnswered = answeredQuestions && answeredQuestions.length === (exercise ? exercise.questionCount : 0)
+  const isAnswered =
+    answeredQuestions && answeredQuestions.length === (exercise ? exercise.questionCount : 0)
 
   return {
     id: exercise ? exercise.id : 0,
