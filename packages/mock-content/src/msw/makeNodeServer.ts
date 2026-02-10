10dc0f7878b0bba4abe72f9905b32b4187448a8b
Mirko Dietrich <dietrich@math.tu-berlin.de>
2026-02-10 01:32:07 +0100

    chore(mock-content): skip `node/getRouteManager` for now

--- packages/mock-content/src/msw/makeNodeServer.ts
+++ packages/mock-content/src/msw/makeNodeServer.ts
@@ -2,14 +2,15 @@
 import { setupServer } from 'msw/node' // https://github.com/mswjs/msw/issues/1786
 
 import config from '@innodoc/config'
-import getRouteManager from '@innodoc/routes/node/getRouteManager'
+// import getRouteManager from '@innodoc/routes/node/getRouteManager'
 
 import getHandlers from './getHandlers'
 
 const makeNodeServer = (baseUrl: string) => {
-  const handlers = getHandlers(baseUrl, getRouteManager(config))
-  const server = setupServer(...handlers)
-  return server
+  // FIXME: node/getRouteManager ??
+  //  const handlers = getHandlers(baseUrl, getRouteManager(config))
+  //  const server = setupServer(...handlers)
+  //  return server
 }
 
 export default makeNodeServer
