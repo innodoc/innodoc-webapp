67e98dcb230fd948832d215135c613144e87d7af
Mirko Dietrich <dietrich@math.tu-berlin.de>
2026-05-13 18:15:42 +0200

    refactor(frontend): extract route registry into RoutesSwitch component
    Move the route name → component mapping from App.tsx into a dedicated
    RoutesSwitch component in @innodoc/ui-features with a type-safe
    RouteRegistry that covers all FrontendRouteName entries.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,52 +1,14 @@
 import { I18nextProvider } from 'react-i18next'
 import { Provider as ReduxProvider } from 'react-redux'
-import { Route, Router, Switch } from 'wouter'
+import { Router } from 'wouter'
 import type { EmotionCache } from '@emotion/react'
 import type I18n from 'i18next'
 
 import PageShell from '@innodoc/ui-features/page-shell'
+import RoutesSwitch from '@innodoc/ui-features/routes-switch'
 import { RouteManagerProvider } from '@innodoc/ui-shared/contexts'
 import type { RouteManager } from '@innodoc/shared-core/routes'
-import type { FrontendRouteName } from '@innodoc/shared-core/types'
 import type { Store } from '@innodoc/ui-store/types'
-
-interface PageComponentProps {
-  routeName: FrontendRouteName
-}
-
-function PageComponent({ routeName }: PageComponentProps) {
-  switch (routeName) {
-    case 'app:index': {
-      return 'Index Page TMP'
-      // return <IndexPage />
-    }
-    case 'app:course:index': {
-      return 'app:course:index'
-    }
-    // default: {
-    //   return <ErrorPage is404 />
-    // }
-  }
-
-  return null
-}
-
-interface RoutesSwitchProps {
-  routeManager: RouteManager
-}
-
-function RoutesSwitch({ routeManager }: RoutesSwitchProps) {
-  const frontendRoutes = routeManager.getFrontendRoutes()
-  const routeEntries = Object.entries(frontendRoutes) as [FrontendRouteName, string][]
-
-  const routes = routeEntries.map(([name, path]) => (
-    <Route key={name} path={path}>
-      <PageComponent routeName={name} />
-    </Route>
-  ))
-
-  return <Switch>{routes}</Switch>
-}
 
 interface AppProps {
   emotionCache: EmotionCache
---
+++ packages/ui-features/src/pages/RoutesSwitch.tsx
@@ -0,0 +1,60 @@
+import { Route, Switch } from 'wouter'
+import type { ComponentType } from 'react'
+
+import type { RouteManager } from '@innodoc/shared-core/routes'
+import type { FrontendRouteName } from '@innodoc/shared-core/types'
+
+import CourseContentPage from './course/CourseContentPage.js'
+import CourseProgressPage from './course/CourseProgressPage.js'
+import CourseSectionPage from './course/CourseSectionPage/CourseSectionPage.js'
+import CourseTocPage from './course/CourseTocPage.js'
+import ErrorPage from './ErrorPage.js'
+import IndexPage from './IndexPage.js'
+import LoginPage from './user/LoginPage.js'
+
+interface RouteEntry<C extends ComponentType = ComponentType> {
+  component: C
+}
+
+type RouteRegistry = Record<FrontendRouteName, RouteEntry>
+
+const routeRegistry: RouteRegistry = {
+  'app:index': { component: IndexPage },
+  'app:course:index': { component: CourseContentPage },
+  'app:course:glossary': { component: () => null }, // TODO
+  'app:course:page': { component: CourseSectionPage },
+  'app:course:progress': { component: CourseProgressPage },
+  'app:course:section': { component: CourseSectionPage },
+  'app:course:toc': { component: CourseTocPage },
+  'app:user:login': { component: LoginPage },
+  'app:user:forgot-password': { component: ErrorPage }, // TODO
+  'app:user:sign-up': { component: ErrorPage }, // TODO
+}
+
+interface RoutesSwitchProps {
+  routeManager: RouteManager
+}
+
+interface PageComponentProps {
+  routeName: FrontendRouteName
+}
+
+function PageComponent({ routeName }: PageComponentProps) {
+  const Component = routeRegistry[routeName].component
+  return <Component />
+}
+
+function RoutesSwitch({ routeManager }: RoutesSwitchProps) {
+  const frontendRoutes = routeManager.getFrontendRoutes()
+  const routeEntries = Object.entries(frontendRoutes) as [FrontendRouteName, string][]
+
+  const routes = routeEntries.map(([name, path]) => (
+    <Route key={name} path={path}>
+      <PageComponent routeName={name} />
+    </Route>
+  ))
+
+  return <Switch>{routes}</Switch>
+}
+
+export default RoutesSwitch
