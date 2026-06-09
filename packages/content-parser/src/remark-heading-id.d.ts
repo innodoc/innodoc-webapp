2507b33039297174600cee96dbc9f245eaf1e8e3
Mirko Dietrich <dietrich@math.tu-berlin.de>
2026-06-09 20:39:22 +0200

    chore(content-parser): use @types/remark-heading-id instead of manual types

--- packages/content-parser/src/remark-heading-id.d.ts
+++
@@ -1,4 +0,0 @@
-// Fix missing type declarations
-declare module 'remark-heading-id' {
-  export default function remarkHeadingId(): import('unified').Transformer<import('mdast').Root, import('mdast').Root>
-}
